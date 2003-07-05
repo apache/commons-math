aa40a7e7ceee3d6a2d017a017a6c456c640dd429
Mark R. Diggory <mdiggory@apache.org>
2003-07-05 18:23:52 +0000

    This commit adds the individual UnivariateStatistic package hierarchy, this hierarchy is currently independent of the Univariate and StoreUnivariate Implementations and provides storeless and storage based mechanisms for working with the individual UnivariateStatistic implementations.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/math/trunk@140961 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/math/stat/univariate/StorelessUnivariateStatistic.java
@@ -0,0 +1,105 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Commons", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+package org.apache.commons.math.stat.univariate;
+
+/**
+ * StorelessUnivariate interface provides methods to increment and access
+ * the internal state of the Statistic.
+ *
+ * @author Mark Diggory
+ */
+public interface StorelessUnivariateStatistic extends UnivariateStatistic {
+
+    /**
+     * Increments the internal state of the Storagless
+     * Implementation.
+     * @param d is the value to increment the state by.
+     * @return the new state of this Statistic. Returns 
+     * the same value as <code>getValue()</code>, Double.NaN if it
+     * has been cleared or just instantiated.
+     */
+    public double increment(double d);
+
+    /**
+     * Returns the current state of the statistic after the
+     * last increment.
+     * @return values of the statistic, Double.NaN if it
+     * has been cleared or just instantiated.
+     */
+    public double getValue();
+
+    /**
+     * Clears all the internal state of the Statistic
+     */
+    public void clear();
+
+    /**
+     * Returns the behavior of this statistic when evaluating
+     * double[]'s using <code>evaluate(double[], int, int)</code> and
+     * <code>evaluate(double[])</code> methods.
+     * @return the state
+     */
+    public boolean isClearOnEval();
+
+    /**
+     * Sets the behavior of this statistic when evaluating
+     * double[]'s using <code>evaluate(double[], int, int)</code> and
+     * <code>evaluate(double[])</code> methods. if true, the internal state
+     * will be cleared between evaluations, otherwise it will be
+     * incrimented.
+     * @param b true | false
+     */
+    public void setClearOnEval(boolean b);
+
+}
