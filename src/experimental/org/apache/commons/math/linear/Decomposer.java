4683ebbf939a0f49f919b2166be963c88f535bf9
Mark R. Diggory <mdiggory@apache.org>
2003-11-20 04:22:17 +0000

    I've thought about this and I like the idea of getting some of this code available in the cvs for experimentation, I'm going ahead and adding it for others to look at and consider.
    This directory will not get compiled into the regular build process or future distributions. We can work on an ant script to provide easy build of this if others think it neccessary.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/math/trunk@141041 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/experimental/org/apache/commons/math/linear/Decomposer.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Nov 19, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.apache.commons.math.linear;
+
+/**
+ * @author Administrator
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public interface Decomposer {
+
+    Decomposition decompose(RealMatrix);
+    
+}
---
+++ src/experimental/org/apache/commons/math/linear/Decomposition.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Nov 19, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.apache.commons.math.linear;
+
+/**
+ * @author Administrator
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public interface Decomposition {
+
+      // solve A*x=b
+      RealVector solve(RealVector b);
+
+
+      // solve matrix equation
+      RealMatrix solve(RealMatrix b);
+
+
+      RealMatrix invert();
+    
+}
