063bb6b88052fce0a015ad7944b2e00038033205
Brent Worden <brentworden@apache.org>
2004-04-27 16:51:52 +0000

    Unit tests.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/math/trunk@141202 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/math/linear/MatrixIndexExceptionTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2003-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.math.linear;
+
+import junit.framework.TestCase;
+
+/**
+ * @version $Revision: 1.1 $ $Date: 2004/04/27 16:51:52 $
+ */
+public class MatrixIndexExceptionTest extends TestCase {
+    /**
+     * 
+     */
+    public void testConstructor(){
+        MatrixIndexException ex = new MatrixIndexException();
+        assertNull(ex.getCause());
+        assertNull(ex.getMessage());
+    }
+    
+    /**
+     * 
+     */
+    public void testConstructorMessage(){
+        String msg = "message";
+        MatrixIndexException ex = new MatrixIndexException(msg);
+        assertNull(ex.getCause());
+        assertEquals(msg, ex.getMessage());
+    }
+    
+    /**
+     * 
+     */
+    public void testConstructorMessageCause(){
+        String outMsg = "outer message";
+        String inMsg = "inner message";
+        Exception cause = new Exception(inMsg);
+        MatrixIndexException ex = new MatrixIndexException(outMsg, cause);
+        assertEquals(outMsg, ex.getMessage());
+        assertEquals(cause, ex.getCause());
+    }
+    
+    /**
+     * 
+     */
+    public void testConstructorCause(){
+        String inMsg = "inner message";
+        Exception cause = new Exception(inMsg);
+        MatrixIndexException ex = new MatrixIndexException(cause);
+        assertEquals(cause, ex.getCause());
+    }
+}
