29c4a34635b527ad2af2c29999f90fac5bad2578
Phil Steitz <psteitz@apache.org>
2004-07-04 22:10:02 +0000

    Added evaluate method taking mean as a parameter. Changed implementation to wrap, rather than extend Variance.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/math/trunk@141362 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/math/stat/univariate/moment/StandardDeviation.java
+++ src/java/org/apache/commons/math/stat/univariate/moment/StandardDeviation.java
@@ -17,28 +17,35 @@ package org.apache.commons.math.stat.univariate.moment;
 
 import java.io.Serializable;
 
+import org.apache.commons.math.stat.univariate.AbstractStorelessUnivariateStatistic;
+
 /**
  * Computes the sample standard deviation.  The standard deviation
  * is the positive square root of the variance.  See {@link Variance} for
- * more information.
+ * more information.  This implementation wraps a {@link Variance}
+ * instance.
  * <p>
  * <strong>Note that this implementation is not synchronized.</strong> If 
  * multiple threads access an instance of this class concurrently, and at least
  * one of the threads invokes the <code>increment()</code> or 
  * <code>clear()</code> method, it must be synchronized externally.
  * 
- * @version $Revision: 1.19 $ $Date: 2004/07/04 09:02:36 $
+ * @version $Revision: 1.20 $ $Date: 2004/07/04 22:10:02 $
  */
-public class StandardDeviation extends Variance implements Serializable {
+public class StandardDeviation extends AbstractStorelessUnivariateStatistic
+    implements Serializable {
 
     /** Serializable version identifier */
-    static final long serialVersionUID = 5728716329662425188L;    
+    static final long serialVersionUID = 5728716329662425188L;  
+    
+    /** Wrapped Variance instance */
+    protected Variance variance = null;
 
     /**
      * Constructs a StandardDeviation
      */
     public StandardDeviation() {
-        super();
+        variance = new Variance();
     }
 
     /**
@@ -47,36 +54,66 @@ public class StandardDeviation extends Variance implements Serializable {
      * @param m2 the external moment
      */
     public StandardDeviation(final SecondMoment m2) {
-        super(m2);
+        variance = new Variance(m2);
     }
 
     /**
      * @see org.apache.commons.math.stat.univariate.StorelessUnivariateStatistic#increment(double)
      */
     public void increment(final double d) {
-        super.increment(d);
+        variance.increment(d);
+    }
+    
+    /**
+     * @see org.apache.commons.math.stat.univariate.StorelessUnivariateStatistic#getN()
+     */
+    public double getN() {
+        return variance.getN();
     }
 
     /**
      * @see org.apache.commons.math.stat.univariate.StorelessUnivariateStatistic#getResult()
      */
     public double getResult() {
-        return Math.sqrt(super.getResult());
+        return Math.sqrt(variance.getResult());
     }
 
     /**
      * @see org.apache.commons.math.stat.univariate.StorelessUnivariateStatistic#clear()
      */
     public void clear() {
-        super.clear();
+        variance.clear();
     }
 
+    /**
+     * Returns the Standard Deviation of the entries in the input array, or 
+     * <code>Double.NaN</code> if the array is empty.
+     * <p>
+     * Returns 0 for a single-value (i.e. length = 1) sample.
+     * <p>
+     * Throws <code>IllegalArgumentException</code> if the array is null.
+     * <p>
+     * Does not change the internal state of the statistic.
+     * 
+     * @param values the input array
+     * @return the standard deviation of the values or Double.NaN if length = 0
+     * @throws IllegalArgumentException if the array is null
+     */  
+    public double evaluate(final double[] values)  {
+        return Math.sqrt(variance.evaluate(values));
+    }
+    
+    
     /**
      * Returns the Standard Deviation of the entries in the specified portion of
      * the input array, or <code>Double.NaN</code> if the designated subarray
      * is empty.
      * <p>
      * Returns 0 for a single-value (i.e. length = 1) sample.
+     * <p>
+     * Throws <code>IllegalArgumentException</code> if the array is null.
+     * <p>
+     * Does not change the internal state of the statistic.
      * 
      * @param values the input array
      * @param begin index of the first array element to include
@@ -86,7 +123,30 @@ public class StandardDeviation extends Variance implements Serializable {
      *  parameters are not valid
      */
     public double evaluate(final double[] values, final int begin, final int length)  {
-       return Math.sqrt(super.evaluate(values, begin, length));
+       return Math.sqrt(variance.evaluate(values, begin, length));
     }
-
+    
+    /**
+     * Returns the Standard Deviation of the entries in the specified portion of
+     * the input array, using the precomputed mean value.  Returns
+     * <code>Double.NaN</code> if the designated subarray is empty.
+     * <p>
+     * Returns 0 for a single-value (i.e. length = 1) sample.
+     * <p>
+     * Throws <code>IllegalArgumentException</code> if the array is null.
+     * <p>
+     * Does not change the internal state of the statistic.
+     * 
+     * @param values the input array
+     * @param mean the precomputed mean value
+     * @param begin index of the first array element to include
+     * @param length the number of elements to include
+     * @return the standard deviation of the values or Double.NaN if length = 0
+     * @throws IllegalArgumentException if the array is null or the array index
+     *  parameters are not valid
+     */
+    public double evaluate(final double[] values, final double mean,
+            final int begin, final int length)  {
+        return Math.sqrt(variance.evaluate(values, mean, begin, length));
+    }
 }
