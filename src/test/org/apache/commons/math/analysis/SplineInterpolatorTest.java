0b61f84404201c71613aa629836fd4e58f04b8a1
Brent Worden <brentworden@apache.org>
2005-04-01 20:49:27 +0000

    updating copyright
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/math/trunk@159729 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/math/analysis/SplineInterpolatorTest.java
+++ src/test/org/apache/commons/math/analysis/SplineInterpolatorTest.java
@@ -1,6 +1,6 @@
 /*
  * 
- * Copyright (c) 2004 The Apache Software Foundation. All rights reserved.
+ * Copyright (c) 2004-2005 The Apache Software Foundation. All rights reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
