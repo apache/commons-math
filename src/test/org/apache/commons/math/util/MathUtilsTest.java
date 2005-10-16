bdf15c7745c4bf2c8e8b6bbed3ff32d482bac8f3
Phil Steitz <psteitz@apache.org>
2005-10-16 00:09:16 +0000

    Added missing fail statements to some MathUtils tests. PR# 37095 Reported by elharo at metalab dot unc dot edu
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/math/branches/MATH_1_1@321515 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/math/util/MathUtilsTest.java
+++ src/test/org/apache/commons/math/util/MathUtilsTest.java
@@ -46,9 +46,11 @@ public final class MathUtilsTest extends TestCase {
         assertEquals(big, MathUtils.addAndCheck(big, 0));
         try {
             int res = MathUtils.addAndCheck(big, 1);
+            fail("Expecting ArithmeticException");
         } catch (ArithmeticException ex) {}
         try {
             int res = MathUtils.addAndCheck(bigNeg, -1);
+            fail("Expecting ArithmeticException");
         } catch (ArithmeticException ex) {}
     }
     
@@ -58,9 +60,11 @@ public final class MathUtilsTest extends TestCase {
         assertEquals(big, MathUtils.mulAndCheck(big, 1));
         try {
             int res = MathUtils.mulAndCheck(big, 2);
+            fail("Expecting ArithmeticException");
         } catch (ArithmeticException ex) {}
         try {
             int res = MathUtils.mulAndCheck(bigNeg, 2);
+            fail("Expecting ArithmeticException");
         } catch (ArithmeticException ex) {}
     }
     
@@ -70,9 +74,11 @@ public final class MathUtilsTest extends TestCase {
         assertEquals(big, MathUtils.subAndCheck(big, 0));
         try {
             int res = MathUtils.subAndCheck(big, -1);
+            fail("Expecting ArithmeticException");
         } catch (ArithmeticException ex) {}
         try {
             int res = MathUtils.subAndCheck(bigNeg, 1);
+            fail("Expecting ArithmeticException");
         } catch (ArithmeticException ex) {}
     }
     
@@ -81,6 +87,7 @@ public final class MathUtilsTest extends TestCase {
         int bigNeg = Integer.MIN_VALUE;
         try {
             int res = MathUtils.subAndCheck(big, -1);
+            fail("Expecting ArithmeticException");
         } catch (ArithmeticException ex) {
             assertEquals("overflow: subtract", ex.getMessage());
         }
