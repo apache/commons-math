571b69346348ca0a9ffb7f212e6c5af56be2e4f7
Luc Maisonobe <luc@apache.org>
2007-01-15 20:58:51 +0000

    added missing files from previous patches (JIRA:MATH-161 and JIRA:MATH-162)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/math/trunk/src/mantissa@496489 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests-src/org/spaceroots/mantissa/ode/StepProblem.java
@@ -0,0 +1,44 @@
+package org.spaceroots.mantissa.ode;
+
+
+public class StepProblem
+  implements FirstOrderDifferentialEquations, SwitchingFunction {
+
+  public StepProblem(double rateBefore, double rateAfter,
+                     double switchTime) {
+    this.rateAfter  = rateAfter;
+    this.switchTime = switchTime;
+    setRate(rateBefore);
+  }
+
+  public void computeDerivatives(double t, double[] y, double[] yDot) {
+    yDot[0] = rate;
+  }
+
+  public int getDimension() {
+    return 1;
+  }
+
+  public void setRate(double rate) {
+    this.rate = rate;
+  }
+
+  public int eventOccurred(double t, double[] y) {
+    setRate(rateAfter);
+    return RESET_DERIVATIVES;
+  }
+
+  public double g(double t, double[] y) {
+    return t - switchTime;
+  }
+
+  public void resetState(double t, double[] y) {
+  }
+
+  private double rate;
+  private double rateAfter;
+  private double switchTime;
+
+  private static final long serialVersionUID = 7590601995477504318L;
+
+}
