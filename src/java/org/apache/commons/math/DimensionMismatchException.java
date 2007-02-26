4b1994a2ff9ee572271d0ed4fe54943330e23c1f
Luc Maisonobe <luc@apache.org>
2007-02-26 23:06:24 +0000

    specialized exception for mismatched vectors or matrices dimensions
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/math/trunk@512064 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/math/DimensionMismatchException.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.math;
+
+/**
+ * Error thrown when two dimensions differ.
+ * @version $Revision:$ $Date$
+ */
+public class DimensionMismatchException extends MathException {
+    
+    /** Serializable version identifier */
+    private static final long serialVersionUID = -1316089546353786411L;
+
+    /**
+     * Construct an exception from the mismatched dimensions
+     * @param dimension1 first dimension
+     * @param dimension2 second dimension
+     */
+    public DimensionMismatchException(int dimension1, int dimension2) {
+        super("dimension mismatch {0} != {1}",
+              new Object[] {
+                new Integer(dimension1), new Integer(dimension2)
+              });
+        this.dimension1 = dimension1;
+        this.dimension2 = dimension2;
+    }
+
+    /**
+     * Get the first dimension
+     * @return first dimension
+     */
+    public int getDimension1() {
+        return dimension1;
+    }
+    
+    /**
+     * Get the second dimension
+     * @return second dimension
+     */
+    public int getDimension2() {
+        return dimension2;
+    }
+
+    /** First dimension. */
+    private int dimension1;
+    
+    /** Second dimension. */
+    private int dimension2;
+    
+}
