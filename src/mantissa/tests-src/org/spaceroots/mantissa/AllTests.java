326ec7313e09ea9c5ed78ec24938f9d8f02c084e
Luc Maisonobe <luc@apache.org>
2007-02-27 20:51:59 +0000

    removed remnants of code transfered from Mantissa to commons-math
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/math/trunk@512385 13f79535-47bb-0310-9956-ffa450edef68

--- src/mantissa/tests-src/org/spaceroots/mantissa/AllTests.java
+++ src/mantissa/tests-src/org/spaceroots/mantissa/AllTests.java
@@ -27,16 +27,12 @@ public class AllTests {
     TestSuite suite= new TestSuite("org.spaceroots.mantissa"); 
 
     suite.addTest(org.spaceroots.mantissa.linalg.AllTests.suite()); 
-    suite.addTest(org.spaceroots.mantissa.estimation.AllTests.suite()); 
     suite.addTest(org.spaceroots.mantissa.functions.AllTests.suite()); 
     suite.addTest(org.spaceroots.mantissa.roots.AllTests.suite()); 
     suite.addTest(org.spaceroots.mantissa.fitting.AllTests.suite()); 
-    suite.addTest(org.spaceroots.mantissa.ode.AllTests.suite()); 
     suite.addTest(org.spaceroots.mantissa.quadrature.AllTests.suite()); 
     suite.addTest(org.spaceroots.mantissa.utilities.AllTests.suite()); 
-    suite.addTest(org.spaceroots.mantissa.geometry.AllTests.suite()); 
     suite.addTest(org.spaceroots.mantissa.algebra.AllTests.suite()); 
-    suite.addTest(org.spaceroots.mantissa.optimization.AllTests.suite()); 
 
     return suite; 
 
