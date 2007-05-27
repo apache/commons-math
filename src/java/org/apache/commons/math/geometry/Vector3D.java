db9790e57e0396c9a0caf1d6faca4a4f4a1aa2c7
Phil Steitz <psteitz@apache.org>
2007-05-27 18:35:14 +0000

    Made license headers conistent, removed author tags (with permission)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/math/trunk@542017 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/math/geometry/Vector3D.java
+++ src/java/org/apache/commons/math/geometry/Vector3D.java
@@ -1,19 +1,19 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-// 
-//   http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.apache.commons.math.geometry;
 
@@ -22,7 +22,6 @@ import java.io.Serializable;
 /** This class implements vectors in a three-dimensional space.
  * <p>Instance of this class are guaranteed to be immutable.</p>
  * @version $Id: Vector3D.java 1716 2006-12-13 22:56:35Z luc $
- * @author L. Maisonobe
  */
 
 public class Vector3D
