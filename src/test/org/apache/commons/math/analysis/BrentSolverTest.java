9232ea98fc333c8ede952406d3f70da8ae89682a
Brent Worden <brentworden@apache.org>
2008-05-07 13:36:33 +0000

    MATH-204.  added root checks for the endpoints.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@654100 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/math/analysis/BrentSolverTest.java
+++ src/test/org/apache/commons/math/analysis/BrentSolverTest.java
@@ -268,6 +268,18 @@ public final class BrentSolverTest extends TestCase {
         assertEquals(result, 1.0, 1E-6);
     }
     
+    public void testRootEndpoints() throws Exception {
+        UnivariateRealFunction f = new SinFunction();
+        UnivariateRealSolver solver = new BrentSolver(f);
+        
+        // endpoint is root
+        double result = solver.solve(Math.PI, 4);
+        assertEquals(result, Math.PI, solver.getAbsoluteAccuracy());
+
+        result = solver.solve(3, Math.PI);
+        assertEquals(result, Math.PI, solver.getAbsoluteAccuracy());
+    }
+    
     public void testBadEndpoints() throws Exception {
         UnivariateRealFunction f = new SinFunction();
         UnivariateRealSolver solver = new BrentSolver(f);
