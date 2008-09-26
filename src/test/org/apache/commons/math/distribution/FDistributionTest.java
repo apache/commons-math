57e0c33a52a0f1e641161e6bf1278e63ee6384cc
Brent Worden <brentworden@apache.org>
2008-09-26 03:09:53 +0000

    MATH-227.  fixed F distribution inverse CDF computation for small denominator degrees of freedom.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@699157 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/math/distribution/FDistributionTest.java
+++ src/test/org/apache/commons/math/distribution/FDistributionTest.java
@@ -105,4 +105,18 @@ public class FDistributionTest extends ContinuousDistributionAbstractTest {
         double x = fd.inverseCumulativeProbability(p);
         assertEquals(.999, x, 1.0e-5);
     }
+
+    public void testSmallDegreesOfFreedom() throws Exception {
+        org.apache.commons.math.distribution.FDistributionImpl fd =
+            new org.apache.commons.math.distribution.FDistributionImpl(
+                1.0, 1.0);
+        double p = fd.cumulativeProbability(0.975);
+        double x = fd.inverseCumulativeProbability(p);
+        assertEquals(0.975, x, 1.0e-5);
+
+        fd.setDenominatorDegreesOfFreedom(2.0);
+        p = fd.cumulativeProbability(0.975);
+        x = fd.inverseCumulativeProbability(p);
+        assertEquals(0.975, x, 1.0e-5);
+    }
 }
