59434c2dce49e0104b4c7d51b7502e4eae7d2773
Luc Maisonobe <luc@apache.org>
2009-06-03 09:06:08 +0000

    Fixed a wrong check for basic variables JIRA: MATH-273
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@781304 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/math/optimization/linear/SimplexTableau.java
+++ src/java/org/apache/commons/math/optimization/linear/SimplexTableau.java
@@ -272,12 +272,10 @@ class SimplexTableau implements Serializable {
     private Integer getBasicRow(final int col) {
         Integer row = null;
         for (int i = getNumObjectiveFunctions(); i < getHeight(); i++) {
-            if (!MathUtils.equals(getEntry(i, col), 0.0, epsilon)) {
-                if (row == null) {
-                    row = i;
-                } else {
-                    return null;
-                }
+            if (MathUtils.equals(getEntry(i, col), 1.0, epsilon) && (row == null)) {
+                row = i;
+            } else if (!MathUtils.equals(getEntry(i, col), 0.0, epsilon)) {
+                return null;
             }
         }
         return row;
