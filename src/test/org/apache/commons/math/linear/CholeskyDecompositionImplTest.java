edb1f394c0fdfc5865be8dec38d9084a04681799
Luc Maisonobe <luc@apache.org>
2009-06-04 20:18:50 +0000

    removed a print
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@781847 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/math/linear/CholeskyDecompositionImplTest.java
+++ src/test/org/apache/commons/math/linear/CholeskyDecompositionImplTest.java
@@ -68,14 +68,13 @@ public class CholeskyDecompositionImplTest {
     /** test non positive definite matrix */
     @Test(expected = NotPositiveDefiniteMatrixException.class)
     public void testNotPositiveDefinite() throws MathException {
-        CholeskyDecomposition cd = new CholeskyDecompositionImpl(MatrixUtils.createRealMatrix(new double[][] {
+        new CholeskyDecompositionImpl(MatrixUtils.createRealMatrix(new double[][] {
                 { 14, 11, 13, 15, 24 },
                 { 11, 34, 13, 8,  25 },
                 { 13, 13, 14, 15, 21 },
                 { 15, 8,  15, 18, 23 },
                 { 24, 25, 21, 23, 45 }
         }));
-        System.out.println(cd.getL().multiply(cd.getLT()));
     }
 
     @Test(expected = NotPositiveDefiniteMatrixException.class)
