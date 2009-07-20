04749a6a8140f1acee2f6c8ddc662655764d7750
Luc Maisonobe <luc@apache.org>
2009-07-20 16:48:30 +0000

    handle serialization of ComplexField (which is a singleton) properly and test it
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@795911 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/math/complex/ComplexField.java
+++ src/java/org/apache/commons/math/complex/ComplexField.java
@@ -31,8 +31,6 @@ import org.apache.commons.math.Field;
  * @since 2.0
  */
 public class ComplexField implements Field<Complex>, Serializable  {
-    // TODO: Add Serializable documentation
-    // TODO: Check Serializable implementation
 
     /** Serializable version identifier. */
     private static final long serialVersionUID = -6130362688700788798L;
@@ -65,6 +63,14 @@ public class ComplexField implements Field<Complex>, Serializable  {
     private static class LazyHolder {
         /** Cached field instance. */
         private static final ComplexField INSTANCE = new ComplexField();
+    }
+
+    /** Handle deserialization of the singleton.
+     * @return the singleton instance
+     */
+    private Object readResolve() {
+        // return the singleton instance
+        return LazyHolder.INSTANCE; 
     }
 
 }
