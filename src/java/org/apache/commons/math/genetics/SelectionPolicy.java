f93b14635a222617ababbd954aca7dc36b7bb4ca
Phil Steitz <psteitz@apache.org>
2009-07-30 01:11:40 +0000

    tagged 2.0-RC2
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/tags/MATH_2_0_RC2@799145 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/math/genetics/SelectionPolicy.java
+++ src/java/org/apache/commons/math/genetics/SelectionPolicy.java
@@ -20,7 +20,7 @@ package org.apache.commons.math.genetics;
  * Algorithm used to select a chromosome pair from a population.
  * 
  * @since 2.0
- * @version $Revision:$ $Date:$
+ * @version $Revision$ $Date$
  */
 public interface SelectionPolicy {
     /**
