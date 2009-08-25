38983e820763c882e778c7d5c68b673fb45a210e
Luc Maisonobe <luc@apache.org>
2009-08-25 18:07:13 +0000

    fixed an error induced by zero entries in simplex solver JIRA: MATH-288
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@807738 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math/optimization/linear/SimplexSolver.java
+++ src/main/java/org/apache/commons/math/optimization/linear/SimplexSolver.java
@@ -77,9 +77,10 @@ public class SimplexSolver extends AbstractLinearOptimizer {
         double minRatio = Double.MAX_VALUE;
         Integer minRatioPos = null;
         for (int i = tableau.getNumObjectiveFunctions(); i < tableau.getHeight(); i++) {
-            double rhs = tableau.getEntry(i, tableau.getWidth() - 1);
-            if (MathUtils.compareTo(tableau.getEntry(i, col), 0, epsilon) >= 0) {
-                double ratio = rhs / tableau.getEntry(i, col);
+            final double rhs = tableau.getEntry(i, tableau.getWidth() - 1);
+            final double entry = tableau.getEntry(i, col);
+            if (MathUtils.compareTo(entry, 0, epsilon) > 0) {
+                final double ratio = rhs / entry;
                 if (ratio < minRatio) {
                     minRatio = ratio;
                     minRatioPos = i; 
