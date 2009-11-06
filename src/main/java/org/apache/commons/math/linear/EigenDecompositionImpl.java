83f18d5299fbadf91ec792c95ee96bcd916ca9af
Luc Maisonobe <luc@apache.org>
2009-11-06 15:11:57 +0000

    Fixed an index computation error in eigen decomposition. Once again, kudos to Dimitri for debugging this. JIRA: MATH-318
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@833433 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math/linear/EigenDecompositionImpl.java
+++ src/main/java/org/apache/commons/math/linear/EigenDecompositionImpl.java
@@ -1132,7 +1132,7 @@ public class EigenDecompositionImpl implements EigenDecomposition {
     private boolean flipIfWarranted(final int n, final int step) {
         if (1.5 * work[pingPong] < work[4 * (n - 1) + pingPong]) {
             // flip array
-            int j = 4 * n - 1;
+            int j = 4 * (n - 1);
             for (int i = 0; i < j; i += 4) {
                 for (int k = 0; k < 4; k += step) {
                     final double tmp = work[i + k];
