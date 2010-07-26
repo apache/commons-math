c917b3b3fa7b0b3e3288f6529835028f82c6f88a
Gilles Sadowski <erans@apache.org>
2010-07-26 21:47:32 +0000

    MATH-396. Introducing "PowellOptimizer" which uses "BracketFinder". "PowellOptimizerTest" uses "SumSincFunction" which uses "SincFunction".
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@979460 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/commons/math/optimization/general/PowellOptimizer.java
@@ -0,0 +1,250 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.math.optimization.general;
+
+import org.apache.commons.math.ConvergenceException;
+import org.apache.commons.math.FunctionEvaluationException;
+import org.apache.commons.math.analysis.MultivariateRealFunction;
+import org.apache.commons.math.analysis.UnivariateRealFunction;
+import org.apache.commons.math.optimization.univariate.AbstractUnivariateRealOptimizer;
+import org.apache.commons.math.optimization.univariate.BrentOptimizer;
+import org.apache.commons.math.optimization.univariate.BracketFinder;
+import org.apache.commons.math.optimization.GoalType;
+import org.apache.commons.math.optimization.MultivariateRealOptimizer;
+import org.apache.commons.math.optimization.OptimizationException;
+import org.apache.commons.math.optimization.RealPointValuePair;
+import org.apache.commons.math.optimization.SimpleScalarValueChecker;
+
+/**
+ * Powell algorithm.
+ * This code is translated and adapted from the Python version of this
+ * algorithm (as implemented in module {@code optimize.py} v0.5 of
+ * <em>SciPy</em>).
+ *
+ * @version $Revision$ $Date$
+ * @since 2.2
+ */
+public class PowellOptimizer
+    extends AbstractScalarOptimizer {
+    /**
+     * Defautl line search tolerance ({@value}).
+     */
+    public static final double DEFAULT_LINE_SEARCH_TOLERANCE = 1e-7;
+    /**
+     * Line search.
+     */
+    private final LineSearch line;
+
+    /**
+     * Constructor using the default line search tolerance (see the
+     * {@link #PowellOptimizer(double) other constructor}).
+     */
+    public PowellOptimizer() {
+        this(DEFAULT_LINE_SEARCH_TOLERANCE);
+    }
+
+    /**
+     * @param lineSearchTolerance Relative error tolerance for the line search
+     * algorithm ({@link BrentOptimizer}).
+     */
+    public PowellOptimizer(double lineSearchTolerance) {
+        line = new LineSearch(lineSearchTolerance);
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    protected RealPointValuePair doOptimize()
+        throws FunctionEvaluationException,
+               OptimizationException {
+        final GoalType goal = getGoalType();
+        final double[] guess = getStartPoint();
+        final int n = guess.length;
+        
+        final double[][] direc = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            direc[i][i] = 1;
+        }
+        
+        double[] x = guess;
+        double fVal = computeObjectiveValue(x);
+        double[] x1 = x.clone();
+        while (true) {
+            incrementIterationsCounter();
+            
+            double fX = fVal;
+            double fX2 = 0;
+            double delta = 0;
+            int bigInd = 0;
+            double alphaMin = 0;
+            
+            double[] direc1 = new double[n];
+            for (int i = 0; i < n; i++) {
+                direc1 = direc[i];
+            
+                fX2 = fVal;
+            
+                line.search(x, direc1);
+                fVal = line.getValueAtOptimum();
+                alphaMin = line.getOptimum();
+                setNewPointAndDirection(x, direc1, alphaMin);
+       
+                if ((fX2 - fVal) > delta) {
+                    delta = fX2 - fVal;
+                    bigInd = i;
+                }
+            }
+
+            final RealPointValuePair previous = new RealPointValuePair(x1, fX);
+            final RealPointValuePair current = new RealPointValuePair(x, fVal);
+            if (getConvergenceChecker().converged(getIterations(), previous, current)) {
+                switch (goal) {
+                case MINIMIZE:
+                    return (fVal < fX ? current : previous);
+                case MAXIMIZE:
+                    return (fVal > fX ? current : previous);
+                }
+            }
+            
+            double[] x2 = new double[n];
+            for (int i = 0; i < n; i++) {
+                direc1[i] = x[i] - x1[i];
+                x2[i] = 2 * x[i] - x1[i];
+            }
+
+            x1 = x.clone();
+            fX2 = computeObjectiveValue(x2);
+
+            if (fX > fX2) {
+                double t = 2 * (fX + fX2 - 2 * fVal);
+                double temp = fX - fVal - delta;
+                t *= temp * temp;
+                temp = fX - fX2;
+                t -= delta * temp * temp;
+
+                if (t < 0.0) {
+                    line.search(x, direc1);
+                    fVal = line.getValueAtOptimum();
+                    alphaMin = line.getOptimum();
+                    setNewPointAndDirection(x, direc1, alphaMin);
+                    
+                    final int lastInd = n - 1;
+                    direc[bigInd] = direc[lastInd];
+                    direc[lastInd] = direc1;
+                }
+            }
+        }
+    }
+
+    /**
+     * Compute a new point (in the original space) and a new direction
+     * vector, resulting from the line search.
+     * The parameters {@code p} and {@code d} will be changed in-place.
+     *
+     * @param p Point used in the line search.
+     * @param d Direction used in the line search.
+     * @param optimum Optimum found by the line search.
+     */
+    private void setNewPointAndDirection(double[] p,
+                                         double[] d,
+                                         double optimum) {
+        final int n = p.length;
+        for (int i = 0; i < n; i++) {
+            d[i] *= optimum;
+            p[i] += d[i];
+        }
+    }
+
+    /**
+     * Class for finding the minimum of the objective function along a given
+     * direction.
+     */
+    private class LineSearch {
+        /**
+         * Optimizer.
+         */
+        private final AbstractUnivariateRealOptimizer optim = new BrentOptimizer();
+        /**
+         * Automatic bracketing.
+         */
+        private final BracketFinder bracket = new BracketFinder();
+        /**
+         * Value of the optimum.
+         */
+        private double optimum = Double.NaN;
+        /**
+         * Value of the objective function at the optimum.
+         */
+        private double valueAtOptimum = Double.NaN;
+
+        /**
+         * @param tolerance Relative tolerance.
+         */
+        public LineSearch(double tolerance) {
+            optim.setRelativeAccuracy(tolerance);
+            optim.setAbsoluteAccuracy(Math.ulp(1d));
+        }
+
+        /**
+         * Find the minimum of the function {@code f(p + alpha * d)}.
+         *
+         * @param p Starting point.
+         * @param d Search direction.
+         */
+        public void search(final double[] p,
+                           final double[] d)
+            throws OptimizationException {
+            try {
+                final int n = p.length;
+                final UnivariateRealFunction f = new UnivariateRealFunction() {
+                        public double value(double alpha)
+                            throws FunctionEvaluationException {
+
+                            final double[] x = new double[n];
+                            for (int i = 0; i < n; i++) {
+                                x[i] = p[i] + alpha * d[i];
+                            }
+                            return computeObjectiveValue(x);
+                        }
+                    };
+
+                final GoalType goal = getGoalType();
+                bracket.search(f, goal, 0, 1);
+                optimum = optim.optimize(f, goal,
+                                         bracket.getLo(),
+                                         bracket.getHi(),
+                                         bracket.getMid());
+                valueAtOptimum = f.value(optimum);
+            } catch (Exception e) {
+                throw new OptimizationException(e);
+            }
+        }
+
+        /**
+         * @return the optimum.
+         */
+        public double getOptimum() {
+            return optimum;
+        }
+        /**
+         * @return the value of the function at the optimum.
+         */
+        public double getValueAtOptimum() {
+            return valueAtOptimum;
+        }
+    }
+}
---
+++ src/test/java/org/apache/commons/math/optimization/general/PowellOptimizerTest.java
@@ -0,0 +1,153 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.math.optimization.general;
+
+import java.util.Arrays;
+
+import org.apache.commons.math.MathException;
+import org.apache.commons.math.FunctionEvaluationException;
+import org.apache.commons.math.analysis.MultivariateRealFunction;
+import org.apache.commons.math.analysis.SumSincFunction;
+import org.apache.commons.math.optimization.GoalType;
+import org.apache.commons.math.optimization.MultivariateRealOptimizer;
+import org.apache.commons.math.optimization.SimpleScalarValueChecker;
+import org.apache.commons.math.optimization.RealPointValuePair;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test for {@link PowellOptimizer}.
+ */
+public class PowellOptimizerTest {
+
+    @Test
+    public void testSumSinc() throws MathException {
+        final MultivariateRealFunction func = new SumSincFunction(-1);
+
+        int dim = 1;
+        final double[] minPoint = new double[dim];
+        for (int i = 0; i < dim; i++) {
+            minPoint[i] = 0;
+        }
+
+        double[] init = new double[dim];
+
+        // Initial is minimum.
+        for (int i = 0; i < dim; i++) {
+            init[i] = minPoint[i];
+        }
+        doTest(func, minPoint, init,  GoalType.MINIMIZE, 1e-15, 1e-8);
+
+        // Initial is far from minimum.
+        for (int i = 0; i < dim; i++) {
+            init[i] = minPoint[i] + 4;
+        }
+        doTest(func, minPoint, init,  GoalType.MINIMIZE, 1e-15, 1e-8);
+    }
+
+    @Test
+    public void testQuadratic() throws MathException {
+        final MultivariateRealFunction func = new MultivariateRealFunction() {
+                public double value(double[] x)
+                    throws FunctionEvaluationException {
+                    final double a = x[0] - 1;
+                    final double b = x[1] - 1;
+                    return a * a + b * b + 1;
+                }
+            };
+
+        int dim = 2;
+        final double[] minPoint = new double[dim];
+        for (int i = 0; i < dim; i++) {
+            minPoint[i] = 1;
+        }
+
+        double[] init = new double[dim];
+
+        // Initial is minimum.
+        for (int i = 0; i < dim; i++) {
+            init[i] = minPoint[i];
+        }
+        doTest(func, minPoint, init,  GoalType.MINIMIZE, 1e-15, 1e-8);
+
+        // Initial is far from minimum.
+        for (int i = 0; i < dim; i++) {
+            init[i] = minPoint[i] - 20;
+        }
+        doTest(func, minPoint, init, GoalType.MINIMIZE, 1e-15, 1e-8);
+    }
+
+    @Test
+    public void testMaximizeQuadratic() throws MathException {
+        final MultivariateRealFunction func = new MultivariateRealFunction() {
+                public double value(double[] x)
+                    throws FunctionEvaluationException {
+                    final double a = x[0] - 1;
+                    final double b = x[1] - 1;
+                    return -a * a - b * b + 1;
+                }
+            };
+
+        int dim = 2;
+        final double[] maxPoint = new double[dim];
+        for (int i = 0; i < dim; i++) {
+            maxPoint[i] = 1;
+        }
+
+        double[] init = new double[dim];
+
+        // Initial is minimum.
+        for (int i = 0; i < dim; i++) {
+            init[i] = maxPoint[i];
+        }
+        doTest(func, maxPoint, init,  GoalType.MAXIMIZE, 1e-15, 1e-8);
+
+        // Initial is far from minimum.
+        for (int i = 0; i < dim; i++) {
+            init[i] = maxPoint[i] - 20;
+        }
+        doTest(func, maxPoint, init, GoalType.MAXIMIZE, 1e-15, 1e-8);
+    }
+
+    /**
+     * @param func Function to optimize.
+     * @param optimum Expected optimum.
+     * @param init Starting point.
+     * @param goal Minimization or maximization.
+     * @param objTol Tolerance (relative error on the objective function).
+     * @param pointTol Tolerance on the position of the optimum.
+     */
+    private void doTest(MultivariateRealFunction func,
+                        double[] optimum,
+                        double[] init,
+                        GoalType goal,
+                        double objTol,
+                        double pointTol)
+        throws MathException {
+        final MultivariateRealOptimizer optim = new PowellOptimizer();
+        final double relTol = 1e-10;
+        optim.setConvergenceChecker(new SimpleScalarValueChecker(objTol, -1));
+
+        final RealPointValuePair result = optim.optimize(func, goal, init);
+        final double[] found = result.getPoint();
+
+        for (int i = 0, dim = optimum.length; i < dim; i++) {
+            Assert.assertEquals(optimum[i], found[i], pointTol);
+        }
+    }
+}
