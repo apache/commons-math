c87e900a1be7b5f4bc15b94177526650c7020ea9
Luc Maisonobe <luc@apache.org>
2010-10-02 22:08:39 +0000

    backported r1003547 from trunk to branch 2.X Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/branches/MATH_2_X@1003900 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math/distribution/AbstractContinuousDistribution.java
+++ src/main/java/org/apache/commons/math/distribution/AbstractContinuousDistribution.java
@@ -50,7 +50,7 @@ public abstract class AbstractContinuousDistribution
     protected final RandomDataImpl randomData = new RandomDataImpl();
 
     /**
-     * Solver absolute accuracy for inverse cum computation
+     * Solver absolute accuracy for inverse cumulative computation
      * @since 2.1
      */
     private double solverAbsoluteAccuracy = BrentSolver.DEFAULT_ABSOLUTE_ACCURACY;
@@ -220,7 +220,7 @@ public abstract class AbstractContinuousDistribution
     protected abstract double getDomainUpperBound(double p);
 
     /**
-     * Returns the solver absolute accuracy for inverse cum computation.
+     * Returns the solver absolute accuracy for inverse cumulative computation.
      *
      * @return the maximum absolute error in inverse cumulative probability estimates
      * @since 2.1
