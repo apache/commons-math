92569b044121bd6f01492e76fabc22368c24c51d
Gilles Sadowski <erans@apache.org>
2010-11-13 20:34:21 +0000

    Removed workaround that ceased to be necessary.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1034867 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math/analysis/interpolation/BicubicSplineInterpolatingFunction.java
+++ src/main/java/org/apache/commons/math/analysis/interpolation/BicubicSplineInterpolatingFunction.java
@@ -17,7 +17,6 @@
 package org.apache.commons.math.analysis.interpolation;
 
 import org.apache.commons.math.DimensionMismatchException;
-import org.apache.commons.math.exception.FunctionEvaluationException;
 import org.apache.commons.math.analysis.BivariateRealFunction;
 import org.apache.commons.math.exception.NoDataException;
 import org.apache.commons.math.exception.OutOfRangeException;
@@ -240,15 +239,7 @@ public class BicubicSplineInterpolatingFunction
         final double xN = (x - xval[i]) / (xval[i + 1] - xval[i]);
         final double yN = (y - yval[j]) / (yval[j + 1] - yval[j]);
 
-        double result = Double.NaN;
-        try { // Workaround to avoid carrying around "try" blocks for an
-              // exception that will never happen
-            result = partialDerivatives[which][i][j].value(xN, yN);
-        } catch (FunctionEvaluationException e) {
-            // Will never happen
-        }
-
-        return result;
+        return partialDerivatives[which][i][j].value(xN, yN);
     }
 
     /**
