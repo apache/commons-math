9dd33b3ce03530eb28f443cf45d5e2186c66a71f
Sebastian Bazley <sebb@apache.org>
2011-01-06 14:51:16 +0000

    Unused imports
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1055900 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/math/complex/ComplexFormatAbstractTest.java
+++ src/test/java/org/apache/commons/math/complex/ComplexFormatAbstractTest.java
@@ -24,7 +24,6 @@ import java.util.Locale;
 import org.junit.Test;
 import org.junit.Assert;
 
-import org.apache.commons.math.util.CompositeFormat;
 import org.apache.commons.math.util.FastMath;
 import org.apache.commons.math.exception.MathIllegalArgumentException;
 import org.apache.commons.math.exception.NullArgumentException;
