95080b966fec0921f72c5607516bc2e1f402e6c7
Gilles Sadowski <erans@apache.org>
2011-02-03 22:45:06 +0000

    Another unit test.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1067018 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/math/analysis/function/StepFunctionTest.java
+++ src/test/java/org/apache/commons/math/analysis/function/StepFunctionTest.java
@@ -78,4 +78,18 @@ public class StepFunctionTest {
         Assert.assertEquals(51.2, f.value(30), EPS);
         Assert.assertEquals(51.2, f.value(Double.POSITIVE_INFINITY), EPS);
     }
+
+    @Test
+    public void testHeaviside() {   
+        final UnivariateRealFunction h = new StepFunction(new double[] {-1, 0},
+                                                          new double[] {0, 1});
+
+        Assert.assertEquals(0, h.value(Double.NEGATIVE_INFINITY), 0);
+        Assert.assertEquals(0, h.value(-Double.MAX_VALUE), 0);
+        Assert.assertEquals(0, h.value(-2), 0);
+        Assert.assertEquals(0, h.value(-Double.MIN_VALUE), 0);
+        Assert.assertEquals(1, h.value(0), 0);
+        Assert.assertEquals(1, h.value(2), 0);
+        Assert.assertEquals(1, h.value(Double.POSITIVE_INFINITY), 0);
+    }
 }
