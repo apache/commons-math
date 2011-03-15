b6bf8f418ade5fc981f66109c0a2e5af512ac9c0
Gilles Sadowski <erans@apache.org>
2011-03-15 12:15:02 +0000

    MATH-546 Wrong  variable type ("int" instead of "double").
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1081744 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math/stat/clustering/KMeansPlusPlusClusterer.java
+++ src/main/java/org/apache/commons/math/stat/clustering/KMeansPlusPlusClusterer.java
@@ -172,7 +172,7 @@ public class KMeansPlusPlusClusterer<T extends Clusterable<T>> {
         while (resultSet.size() < k) {
             // For each data point x, compute D(x), the distance between x and
             // the nearest center that has already been chosen.
-            int sum = 0;
+            double sum = 0;
             for (int i = 0; i < pointSet.size(); i++) {
                 final T p = pointSet.get(i);
                 final Cluster<T> nearest = getNearestCluster(resultSet, p);
