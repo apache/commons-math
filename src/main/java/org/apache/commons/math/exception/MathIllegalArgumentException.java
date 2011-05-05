1df5ae529c2e21a9ba87ba8a2701994a4c6a3d75
Gilles Sadowski <erans@apache.org>
2011-05-05 12:37:31 +0000

    MATH-566 Created "ExceptionContext" class. Made "MathIllegalArgumentException" inherit from the Java standard "IllegalArgumentException".
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1099771 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math/exception/MathIllegalArgumentException.java
+++ src/main/java/org/apache/commons/math/exception/MathIllegalArgumentException.java
@@ -17,20 +17,24 @@
 package org.apache.commons.math.exception;
 
 import org.apache.commons.math.exception.util.Localizable;
+import org.apache.commons.math.exception.util.ExceptionContext;
+import org.apache.commons.math.exception.util.ExceptionContextProvider;
 
 /**
  * Base class for all preconditions violation exceptions.
  * In most cases, this class should not be instantiated directly: it should
- * serve as a base class to create all the exceptions that share the semantics
- * of the standard {@link IllegalArgumentException}, but must also provide a
- * localized message.
+ * serve as a base class to create all the exceptions that have the semantics
+ * of the standard {@link IllegalArgumentException}.
  *
  * @since 2.2
  * @version $Revision$ $Date$
  */
-public class MathIllegalArgumentException extends MathRuntimeException {
+public class MathIllegalArgumentException extends IllegalArgumentException
+    implements ExceptionContextProvider {
     /** Serializable version Id. */
     private static final long serialVersionUID = -6024911025449780478L;
+    /** Context. */
+    private final ExceptionContext context = new ExceptionContext();
 
     /**
      * @param pattern Message pattern explaining the cause of the error.
@@ -38,6 +42,23 @@ public class MathIllegalArgumentException extends MathRuntimeException {
      */
     public MathIllegalArgumentException(Localizable pattern,
                                         Object ... args) {
-        addMessage(pattern, args);
+        context.addMessage(pattern, args);
+    }
+
+    /** {@inheritDoc} */
+    public ExceptionContext getContext() {
+        return context;
+    }
+
+   /** {@inheritDoc} */
+    @Override
+    public String getMessage() {
+        return context.getMessage();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getLocalizedMessage() {
+        return context.getLocalizedMessage();
     }
 }
