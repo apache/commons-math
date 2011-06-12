apache/commons-math

commit de001e7bcf9acb761047bdcf40f48244f8b63642

Removed deprecated code in implementations of "IntegerDistribution". Added "final" keyword. Added "sample" methods to "IntegerDistribution" and "ContinuousDistribution" interfaces. Cleaned up Javadoc.
git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1003512 13f79535-47bb-0310-9956-ffa450edef68

src/main/java/org/apache/commons/math/distribution/AbstractIntegerDistribution.java
@@ -20,7 +20,9 @@ import java.io.Serializable;
 
 import org.apache.commons.math.FunctionEvaluationException;
 import org.apache.commons.math.MathException;
-import org.apache.commons.math.MathRuntimeException;
+import org.apache.commons.math.exception.NotStrictlyPositiveException;
+import org.apache.commons.math.exception.NumberIsTooSmallException;
+import org.apache.commons.math.exception.OutOfRangeException;
 import org.apache.commons.math.exception.util.LocalizedFormats;
 import org.apache.commons.math.random.RandomDataImpl;
 import org.apache.commons.math.util.FastMath;
@@ -35,12 +37,10 @@ import org.apache.commons.math.util.FastMath;
  */
 public abstract class AbstractIntegerDistribution extends AbstractDistribution
     implements IntegerDistribution, Serializable {
-
    /** Serializable version identifier */
     private static final long serialVersionUID = -1146319659338487221L;
-
     /**
-     * RandomData instance used to generate samples from the distribution
+     * RandomData instance used to generate samples from the distribution.
      * @since 2.2
      */
     protected final RandomDataImpl randomData = new RandomDataImpl();
@@ -48,22 +48,19 @@ public abstract class AbstractIntegerDistribution extends AbstractDistribution
     /**
      * Default constructor.
      */
-    protected AbstractIntegerDistribution() {
-        super();
-    }
+    protected AbstractIntegerDistribution() {}
 
     /**
-     * For a random variable X whose values are distributed according
-     * to this distribution, this method returns P(X &le; x).  In other words,
-     * this method represents the  (cumulative) distribution function, or
-     * CDF, for this distribution.
-     * <p>
-     * If <code>x</code> does not represent an integer value, the CDF is
-     * evaluated at the greatest integer less than x.
+     * For a random variable {@code X} whose values are distributed according
+     * to this distribution, this method returns {@code P(X < x)}.  In other
+     * words, this method represents the (cumulative) distribution function,
+     * or CDF, for this distribution.
+     * If {@code x} does not represent an integer value, the CDF is
+     * evaluated at the greatest integer less than {@code x}.
      *
-     * @param x the value at which the distribution function is evaluated.
-     * @return cumulative probability that a random variable with this
-     * distribution takes a value less than or equal to <code>x</code>
+     * @param x Value at which the distribution function is evaluated.
+     * @return the cumulative probability that a random variable with this
+     * distribution takes a value less than or equal to {@code x}.
      * @throws MathException if the cumulative probability can not be
      * computed due to convergence or other numerical errors.
      */
@@ -72,24 +69,25 @@ public abstract class AbstractIntegerDistribution extends AbstractDistribution
     }
 
     /**
-     * For a random variable X whose values are distributed according
-     * to this distribution, this method returns P(x0 &le; X &le; x1).
+     * For a random variable {@code X} whose values are distributed
+     * according to this distribution, this method returns
+     * {@code P(x0 < X < x1)}.
      *
-     * @param x0 the (inclusive) lower bound
-     * @param x1 the (inclusive) upper bound
+     * @param x0 Inclusive lower bound.
+     * @param x1 Inclusive upper bound.
      * @return the probability that a random variable with this distribution
-     * will take a value between <code>x0</code> and <code>x1</code>,
+     * will take a value between {@code x0} and {@code x1},
      * including the endpoints.
      * @throws MathException if the cumulative probability can not be
      * computed due to convergence or other numerical errors.
-     * @throws IllegalArgumentException if <code>x0 > x1</code>
+     * @throws NumberIsTooSmallException if {@code x1 > x0}.
      */
     @Override
     public double cumulativeProbability(double x0, double x1)
         throws MathException {
-        if (x0 > x1) {
-            throw MathRuntimeException.createIllegalArgumentException(
-                  LocalizedFormats.LOWER_ENDPOINT_ABOVE_UPPER_ENDPOINT, x0, x1);
+        if (x1 < x0) {
+            throw new NumberIsTooSmallException(LocalizedFormats.LOWER_ENDPOINT_ABOVE_UPPER_ENDPOINT,
+                                                x0, x1, true);
         }
         if (FastMath.floor(x0) < x0) {
             return cumulativeProbability(((int) FastMath.floor(x0)) + 1,
@@ -101,27 +99,27 @@ public abstract class AbstractIntegerDistribution extends AbstractDistribution
     }
 
     /**
-     * For a random variable X whose values are distributed according
-     * to this distribution, this method returns P(X &le; x).  In other words,
-     * this method represents the probability distribution function, or PDF,
-     * for this distribution.
+     * For a random variable {@code X} whose values are distributed according
+     * to this distribution, this method returns {@code P(X < x)}. In other
+     * words, this method represents the probability distribution function,
+     * or PDF, for this distribution.
      *
-     * @param x the value at which the PDF is evaluated.
+     * @param x Value at which the PDF is evaluated.
      * @return PDF for this distribution.
      * @throws MathException if the cumulative probability can not be
-     *            computed due to convergence or other numerical errors.
+     * computed due to convergence or other numerical errors.
      */
     public abstract double cumulativeProbability(int x) throws MathException;
 
     /**
-     * For a random variable X whose values are distributed according
-     * to this distribution, this method returns P(X = x). In other words, this
-     * method represents the probability mass function,  or PMF, for the distribution.
-     * <p>
-     * If <code>x</code> does not represent an integer value, 0 is returned.
+     * For a random variable {@code X} whose values are distributed according
+     * to this distribution, this method returns {@code P(X = x)}. In other
+     * words, this method represents the probability mass function, or PMF,
+     * for the distribution.
+     * If {@code x} does not represent an integer value, 0 is returned.
      *
-     * @param x the value at which the probability density function is evaluated
-     * @return the value of the probability density function at x
+     * @param x Value at which the probability density function is evaluated.
+     * @return the value of the probability density function at {@code x}.
      */
     public double probability(double x) {
         double fl = FastMath.floor(x);
@@ -133,39 +131,38 @@ public abstract class AbstractIntegerDistribution extends AbstractDistribution
     }
 
     /**
-    * For a random variable X whose values are distributed according
-     * to this distribution, this method returns P(x0 &le; X &le; x1).
+     * For a random variable {@code X} whose values are distributed according
+     * to this distribution, this method returns {@code P(x0 < X < x1)}.
      *
-     * @param x0 the inclusive, lower bound
-     * @param x1 the inclusive, upper bound
+     * @param x0 Inclusive lower bound.
+     * @param x1 Inclusive upper bound.
      * @return the cumulative probability.
      * @throws MathException if the cumulative probability can not be
-     *            computed due to convergence or other numerical errors.
-     * @throws IllegalArgumentException if x0 > x1
+     * computed due to convergence or other numerical errors.
+     * @throws NumberIsTooSmallException {@code if x0 > x1}.
      */
     public double cumulativeProbability(int x0, int x1) throws MathException {
-        if (x0 > x1) {
-            throw MathRuntimeException.createIllegalArgumentException(
-                  LocalizedFormats.LOWER_ENDPOINT_ABOVE_UPPER_ENDPOINT, x0, x1);
+        if (x1 < x0) {
+            throw new NumberIsTooSmallException(LocalizedFormats.LOWER_ENDPOINT_ABOVE_UPPER_ENDPOINT,
+                                                x0, x1, true);
         }
         return cumulativeProbability(x1) - cumulativeProbability(x0 - 1);
     }
 
     /**
-     * For a random variable X whose values are distributed according
-     * to this distribution, this method returns the largest x, such
-     * that P(X &le; x) &le; <code>p</code>.
+     * For a random variable {@code X} whose values are distributed according
+     * to this distribution, this method returns the largest {@code x}, such
+     * that {@code P(X < x) < p}.
      *
-     * @param p the desired probability
-     * @return the largest x such that P(X &le; x) <= p
+     * @param p Desired probability.
+     * @return the largest {@code x} such that {@code P(X < x) <= p}.
      * @throws MathException if the inverse cumulative probability can not be
-     *            computed due to convergence or other numerical errors.
-     * @throws IllegalArgumentException if p < 0 or p > 1
+     * computed due to convergence or other numerical errors.
+     * @throws OutOfRangeException if {@code p < 0} or {@code p > 1}.
      */
     public int inverseCumulativeProbability(final double p) throws MathException{
-        if (p < 0.0 || p > 1.0) {
-            throw MathRuntimeException.createIllegalArgumentException(
-                  LocalizedFormats.OUT_OF_RANGE_SIMPLE, p, 0.0, 1.0);
+        if (p < 0 || p > 1) {
+            throw new OutOfRangeException(p, 0, 1);
         }
 
         // by default, do simple bisection.
@@ -210,10 +207,7 @@ public abstract class AbstractIntegerDistribution extends AbstractDistribution
     }
 
     /**
-     * Reseeds the random generator used to generate samples.
-     *
-     * @param seed the new seed
-     * @since 2.2
+     * {@inheritDoc}
      */
     public void reseedRandomGenerator(long seed) {
         randomData.reSeed(seed);
@@ -222,29 +216,33 @@ public abstract class AbstractIntegerDistribution extends AbstractDistribution
     /**
      * Generates a random value sampled from this distribution. The default
      * implementation uses the
-     * <a href="http://en.wikipedia.org/wiki/Inverse_transform_sampling"> inversion method.</a>
+     * <a href="http://en.wikipedia.org/wiki/Inverse_transform_sampling">
+     *  inversion method.
+     * </a>
      *
-     * @return random value
+     * @return a random value.
      * @since 2.2
-     * @throws MathException if an error occurs generating the random value
+     * @throws MathException if an error occurs generating the random value.
      */
     public int sample() throws MathException {
         return randomData.nextInversionDeviate(this);
     }
 
     /**
-     * Generates a random sample from the distribution.  The default implementation
-     * generates the sample by calling {@link #sample()} in a loop.
+     * Generates a random sample from the distribution.  The default
+     * implementation generates the sample by calling {@link #sample()}
+     * in a loop.
      *
-     * @param sampleSize number of random values to generate
+     * @param sampleSize number of random values to generate.
      * @since 2.2
-     * @return an array representing the random sample
-     * @throws MathException if an error occurs generating the sample
-     * @throws IllegalArgumentException if sampleSize is not positive
+     * @return an array representing the random sample.
+     * @throws MathException if an error occurs generating the sample.
+     * @throws NotStrictlyPositiveException if {@code sampleSize <= 0}.
      */
     public int[] sample(int sampleSize) throws MathException {
         if (sampleSize <= 0) {
-            MathRuntimeException.createIllegalArgumentException(LocalizedFormats.NOT_POSITIVE_SAMPLE_SIZE, sampleSize);
+            throw new NotStrictlyPositiveException(LocalizedFormats.NUMBER_OF_SAMPLES,
+                                                   sampleSize);
         }
         int[] out = new int[sampleSize];
         for (int i = 0; i < sampleSize; i++) {
@@ -254,16 +252,21 @@ public abstract class AbstractIntegerDistribution extends AbstractDistribution
     }
 
     /**
-     * Computes the cumulative probability function and checks for NaN values returned.
-     * Throws MathException if the value is NaN. Wraps and rethrows any MathException encountered
-     * evaluating the cumulative probability function in a FunctionEvaluationException. Throws
-     * FunctionEvaluationException of the cumulative probability function returns NaN.
+     * Computes the cumulative probability function and checks for NaN
+     * values returned.
+     * Throws MathException if the value is NaN. Wraps and rethrows any
+     * MathException encountered evaluating the cumulative probability
+     * function in a FunctionEvaluationException.
+     * Throws FunctionEvaluationException of the cumulative probability
+     * function returns NaN.
      *
-     * @param argument input value
-     * @return cumulative probability
-     * @throws FunctionEvaluationException if a MathException occurs computing the cumulative probability
+     * @param argument Input value.
+     * @return the cumulative probability.
+     * @throws FunctionEvaluationException if a MathException occurs
+     * computing the cumulative probability.
      */
-    private double checkedCumulativeProbability(int argument) throws FunctionEvaluationException {
+    private double checkedCumulativeProbability(int argument)
+        throws FunctionEvaluationException {
         double result = Double.NaN;
         try {
             result = cumulativeProbability(argument);
@@ -278,24 +281,22 @@ public abstract class AbstractIntegerDistribution extends AbstractDistribution
     }
 
     /**
-     * Access the domain value lower bound, based on <code>p</code>, used to
+     * Access the domain value lower bound, based on {@code p}, used to
      * bracket a PDF root.  This method is used by
      * {@link #inverseCumulativeProbability(double)} to find critical values.
      *
-     * @param p the desired probability for the critical value
-     * @return domain value lower bound, i.e.
-     *         P(X &lt; <i>lower bound</i>) &lt; <code>p</code>
+     * @param p Desired probability for the critical value
+     * @return the domain value lower bound, i.e. {@code P(X < 'lower bound') < p}.
      */
     protected abstract int getDomainLowerBound(double p);
 
     /**
-     * Access the domain value upper bound, based on <code>p</code>, used to
+     * Access the domain value upper bound, based on {@code p}, used to
      * bracket a PDF root.  This method is used by
      * {@link #inverseCumulativeProbability(double)} to find critical values.
      *
-     * @param p the desired probability for the critical value
-     * @return domain value upper bound, i.e.
-     *         P(X &lt; <i>upper bound</i>) &gt; <code>p</code>
+     * @param p Desired probability for the critical value.
+     * @return the domain value upper bound, i.e. {@code P(X < 'upper bound') > p}.
      */
     protected abstract int getDomainUpperBound(double p);
 }

src/main/java/org/apache/commons/math/distribution/BinomialDistributionImpl.java
@@ -19,7 +19,8 @@ package org.apache.commons.math.distribution;
 import java.io.Serializable;
 
 import org.apache.commons.math.MathException;
-import org.apache.commons.math.MathRuntimeException;
+import org.apache.commons.math.exception.OutOfRangeException;
+import org.apache.commons.math.exception.NotPositiveException;
 import org.apache.commons.math.exception.util.LocalizedFormats;
 import org.apache.commons.math.special.Beta;
 import org.apache.commons.math.util.FastMath;
@@ -31,108 +32,55 @@ import org.apache.commons.math.util.FastMath;
  */
 public class BinomialDistributionImpl extends AbstractIntegerDistribution
         implements BinomialDistribution, Serializable {
-
-    /** Serializable version identifier */
+    /** Serializable version identifier. */
     private static final long serialVersionUID = 6751309484392813623L;
-
     /** The number of trials. */
-    private int numberOfTrials;
-
+    private final int numberOfTrials;
     /** The probability of success. */
-    private double probabilityOfSuccess;
+    private final double probabilityOfSuccess;
 
     /**
      * Create a binomial distribution with the given number of trials and
      * probability of success.
      *
-     * @param trials the number of trials.
-     * @param p the probability of success.
+     * @param trials Number of trials.
+     * @param p Probability of success.
+     * @throws NotPositiveException if {@code trials < 0}.
+     * @throws OutOfRangeException if {@code p < 0} or {@code p > 1}.
      */
     public BinomialDistributionImpl(int trials, double p) {
-        super();
-        setNumberOfTrialsInternal(trials);
-        setProbabilityOfSuccessInternal(p);
+        if (trials < 0) {
+            throw new NotPositiveException(LocalizedFormats.NUMBER_OF_TRIALS,
+                                           trials);
+        }
+        if (p < 0 || p > 1) {
+            throw new OutOfRangeException(p, 0, 1);
+        }
+
+        probabilityOfSuccess = p;
+        numberOfTrials = trials;
     }
 
     /**
-     * Access the number of trials for this distribution.
-     *
-     * @return the number of trials.
+     * {@inheritDoc}
      */
     public int getNumberOfTrials() {
         return numberOfTrials;
     }
 
     /**
-     * Access the probability of success for this distribution.
-     *
-     * @return the probability of success.
+     * {@inheritDoc}
      */
     public double getProbabilityOfSuccess() {
         return probabilityOfSuccess;
     }
 
     /**
-     * Change the number of trials for this distribution.
-     *
-     * @param trials the new number of trials.
-     * @throws IllegalArgumentException if <code>trials</code> is not a valid
-     *             number of trials.
-     * @deprecated as of 2.1 (class will become immutable in 3.0)
-     */
-    @Deprecated
-    public void setNumberOfTrials(int trials) {
-        setNumberOfTrialsInternal(trials);
-    }
-    /**
-     * Change the number of trials for this distribution.
-     *
-     * @param trials the new number of trials.
-     * @throws IllegalArgumentException if <code>trials</code> is not a valid
-     *             number of trials.
-     */
-    private void setNumberOfTrialsInternal(int trials) {
-        if (trials < 0) {
-            throw MathRuntimeException.createIllegalArgumentException(
-                    LocalizedFormats.NEGATIVE_NUMBER_OF_TRIALS, trials);
-        }
-        numberOfTrials = trials;
-    }
-
-    /**
-     * Change the probability of success for this distribution.
-     *
-     * @param p the new probability of success.
-     * @throws IllegalArgumentException if <code>p</code> is not a valid
-     *             probability.
-     * @deprecated as of 2.1 (class will become immutable in 3.0)
-     */
-    @Deprecated
-    public void setProbabilityOfSuccess(double p) {
-        setProbabilityOfSuccessInternal(p);
-    }
-    /**
-     * Change the probability of success for this distribution.
-     *
-     * @param p the new probability of success.
-     * @throws IllegalArgumentException if <code>p</code> is not a valid
-     *             probability.
-     */
-    private void setProbabilityOfSuccessInternal(double p) {
-        if (p < 0.0 || p > 1.0) {
-            throw MathRuntimeException.createIllegalArgumentException(
-                    LocalizedFormats.OUT_OF_RANGE_SIMPLE, p, 0.0, 1.0);
-        }
-        probabilityOfSuccess = p;
-    }
-
-    /**
-     * Access the domain value lower bound, based on <code>p</code>, used to
+     * Access the domain value lower bound, based on {@code p}, used to
      * bracket a PDF root.
      *
-     * @param p the desired probability for the critical value
-     * @return domain value lower bound, i.e. P(X &lt; <i>lower bound</i>) &lt;
-     *         <code>p</code>
+     * @param p Desired probability for the critical value.
+     * @return the domain value lower bound, i.e. {@code P(X < 'lower bound') < p}.
      */
     @Override
     protected int getDomainLowerBound(double p) {
@@ -140,12 +88,11 @@ public class BinomialDistributionImpl extends AbstractIntegerDistribution
     }
 
     /**
-     * Access the domain value upper bound, based on <code>p</code>, used to
+     * Access the domain value upper bound, based on {@code p}, used to
      * bracket a PDF root.
      *
-     * @param p the desired probability for the critical value
-     * @return domain value upper bound, i.e. P(X &lt; <i>upper bound</i>) &gt;
-     *         <code>p</code>
+     * @param p Desired probability for the critical value
+     * @return the domain value upper bound, i.e. {@code P(X < 'upper bound') > p}.
      */
     @Override
     protected int getDomainUpperBound(double p) {
@@ -153,12 +100,12 @@ public class BinomialDistributionImpl extends AbstractIntegerDistribution
     }
 
     /**
-     * For this distribution, X, this method returns P(X &le; x).
+     * For this distribution, {@code X}, this method returns {@code P(X < x)}.
      *
-     * @param x the value at which the PDF is evaluated.
+     * @param x Value at which the PDF is evaluated.
      * @return PDF for this distribution.
      * @throws MathException if the cumulative probability can not be computed
-     *             due to convergence or other numerical errors.
+     * due to convergence or other numerical errors.
      */
     @Override
     public double cumulativeProbability(int x) throws MathException {
@@ -175,9 +122,9 @@ public class BinomialDistributionImpl extends AbstractIntegerDistribution
     }
 
     /**
-     * For this distribution, X, this method returns P(X = x).
+     * For this distribution, {@code X}, this method returns {@code P(X = x)}.
      *
-     * @param x the value at which the PMF is evaluated.
+     * @param x Value at which the PMF is evaluated.
      * @return PMF for this distribution.
      */
     public double probability(int x) {
@@ -193,18 +140,15 @@ public class BinomialDistributionImpl extends AbstractIntegerDistribution
     }
 
     /**
-     * For this distribution, X, this method returns the largest x, such that
-     * P(X &le; x) &le; <code>p</code>.
-     * <p>
-     * Returns <code>-1</code> for p=0 and <code>Integer.MAX_VALUE</code> for
-     * p=1.
-     * </p>
+     * For this distribution, {@code X}, this method returns the largest
+     * {@code x}, such that {@code P(X < x) p}.
+     * It will return -1 when p = 0 and {@code Integer.MAX_VALUE} when p = 1.
      *
-     * @param p the desired probability
-     * @return the largest x such that P(X &le; x) <= p
+     * @param p Desired probability.
+     * @return the largest {@code x} such that {@code P(X < x) <= p}.
      * @throws MathException if the inverse cumulative probability can not be
-     *             computed due to convergence or other numerical errors.
-     * @throws IllegalArgumentException if p < 0 or p > 1
+     * computed due to convergence or other numerical errors.
+     * @throws OutOfRangeException if {@code p < 0} or {@code p > 1}.
      */
     @Override
     public int inverseCumulativeProbability(final double p)

commit 72858b844718a263dec4e10514bc9dc080508f1e

Fixed javadoc errors introduced in r1003512 (post 2.2 release).  JIRA: MATH-587
git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1134948 13f79535-47bb-0310-9956-ffa450edef68

src/main/java/org/apache/commons/math/distribution/AbstractIntegerDistribution.java
@@ -51,7 +51,7 @@ public abstract class AbstractIntegerDistribution extends AbstractDistribution
 
     /**
      * For a random variable {@code X} whose values are distributed according
-     * to this distribution, this method returns {@code P(X < x)}.  In other
+     * to this distribution, this method returns {@code P(X <= x)}.  In other
      * words, this method represents the (cumulative) distribution function,
      * or CDF, for this distribution.
      * If {@code x} does not represent an integer value, the CDF is
@@ -70,7 +70,7 @@ public abstract class AbstractIntegerDistribution extends AbstractDistribution
     /**
      * For a random variable {@code X} whose values are distributed
      * according to this distribution, this method returns
-     * {@code P(x0 < X < x1)}.
+     * {@code P(x0 <= X <= x1)}.
      *
      * @param x0 Inclusive lower bound.
      * @param x1 Inclusive upper bound.
@@ -99,7 +99,7 @@ public abstract class AbstractIntegerDistribution extends AbstractDistribution
 
     /**
      * For a random variable {@code X} whose values are distributed according
-     * to this distribution, this method returns {@code P(X < x)}. In other
+     * to this distribution, this method returns {@code P(X <= x)}. In other
      * words, this method represents the probability distribution function,
      * or PDF, for this distribution.
      *

src/main/java/org/apache/commons/math/distribution/BinomialDistributionImpl.java
@@ -100,7 +100,7 @@ public class BinomialDistributionImpl extends AbstractIntegerDistribution
     }
 
     /**
-     * For this distribution, {@code X}, this method returns {@code P(X < x)}.
+     * For this distribution, {@code X}, this method returns {@code P(X <= x)}.
      *
      * @param x Value at which the PDF is evaluated.
      * @return PDF for this distribution.

src/main/java/org/apache/commons/math/distribution/HypergeometricDistributionImpl.java
@@ -87,7 +87,7 @@ public class HypergeometricDistributionImpl extends AbstractIntegerDistribution
     }
 
     /**
-     * For this distribution, {@code X}, this method returns {@code P(X < x)}.
+     * For this distribution, {@code X}, this method returns {@code P(X <= x)}.
      *
      * @param x Value at which the PDF is evaluated.
      * @return PDF for this distribution.

src/main/java/org/apache/commons/math/distribution/IntegerDistribution.java
@@ -37,7 +37,7 @@ public interface IntegerDistribution extends DiscreteDistribution {
 
     /**
      * For a random variable {@code X} whose values are distributed according
-     * to this distribution, this method returns {@code P(X < x)}.  In other
+     * to this distribution, this method returns {@code P(X <= x)}.  In other
      * words, this method represents the probability distribution function, or
      * PDF for the distribution.
      *
@@ -50,7 +50,7 @@ public interface IntegerDistribution extends DiscreteDistribution {
 
     /**
      * For this distribution, {@code X}, this method returns
-     * {@code P(x0 < X < x1)}.
+     * {@code P(x0 <= X <= x1)}.
      *
      * @param x0 the inclusive, lower bound
      * @param x1 the inclusive, upper bound
@@ -63,7 +63,7 @@ public interface IntegerDistribution extends DiscreteDistribution {
 
     /**
      * For this distribution, {@code X}, this method returns the largest
-     * {@code x} such that {@code P(X < x) <= p}.
+     * {@code x} such that {@code P(X <= x) <= p}.
      * <br/>
      * Note that this definition implies:
      * <ul>
@@ -73,7 +73,7 @@ public interface IntegerDistribution extends DiscreteDistribution {
      *   no such value {@code m},  {@code Integer.MIN_VALUE} is returned.
      *  </li>
      *  <li> If there is a maximum value, {@code M}, such that
-     *   {@code P(X < M) = 1}, then {@code M} is returned by
+     *   {@code P(X <= M) = 1}, then {@code M} is returned by
      *   {@code inverseCumulativeProbability(1)}.
      *   If there is no such value, {@code M}, {@code Integer.MAX_VALUE} is
      *   returned.

src/main/java/org/apache/commons/math/distribution/PascalDistributionImpl.java
@@ -100,7 +100,7 @@ public class PascalDistributionImpl extends AbstractIntegerDistribution
     }
 
     /**
-     * For this distribution, {@code X}, this method returns {@code P(X < x)}.
+     * For this distribution, {@code X}, this method returns {@code P(X <= x)}.
      *
      * @param x Value at which the PDF is evaluated.
      * @return PDF for this distribution.
@@ -140,11 +140,11 @@ public class PascalDistributionImpl extends AbstractIntegerDistribution
 
     /**
      * For this distribution, {@code X}, this method returns the largest
-     * {@code x}, such that {@code P(X < x) p}.
+     * {@code x}, such that {@code P(X <= x) <= p}.
      * It will return -1 when p = 0 and {@code Integer.MAX_VALUE} when p = 1.
      *
      * @param p Desired probability.
-     * @return the largest {@code x} such that {@code P(X < x) <= p}.
+     * @return the largest {@code x} such that {@code P(X <= x) <= p}.
      * @throws MathException if the inverse cumulative probability can not be
      * computed due to convergence or other numerical errors.
      * @throws OutOfRangeException if {@code p < 0} or {@code p > 1}.