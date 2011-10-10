5e6389761c6575f815a5ac91a22c4ecc48596729
Luc Maisonobe <luc@apache.org>
2011-10-10 19:56:03 +0000

    Fixed an integer overflow in OpenMapRealMatrix.
    JIRA: MATH-679
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1181181 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math/linear/OpenMapRealMatrix.java
+++ src/main/java/org/apache/commons/math/linear/OpenMapRealMatrix.java
@@ -19,6 +19,7 @@ package org.apache.commons.math.linear;
 
 import java.io.Serializable;
 
+import org.apache.commons.math.exception.NumberIsTooLargeException;
 import org.apache.commons.math.util.OpenIntToDoubleHashMap;
 
 /**
@@ -46,6 +47,11 @@ public class OpenMapRealMatrix extends AbstractRealMatrix
      */
     public OpenMapRealMatrix(int rowDimension, int columnDimension) {
         super(rowDimension, columnDimension);
+        long lRow = (long) rowDimension;
+        long lCol = (long) columnDimension;
+        if (lRow * lCol >= (long) Integer.MAX_VALUE) {
+            throw new NumberIsTooLargeException(lRow * lCol, Integer.MAX_VALUE, false);
+        }
         this.rows = rowDimension;
         this.columns = columnDimension;
         this.entries = new OpenIntToDoubleHashMap(0.0);
