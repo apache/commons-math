7980a24266d463bacc8f1271a83865a7d5638891
Luc Maisonobe <luc@apache.org>
2011-10-26 09:08:22 +0000

    Fixed an event resetting issue in ODE.
    When several discrete events occur during the same ODE integration step,
    they are handled chronologically or reverse chronologically depending on
    the integration direction. If one of the event truncates the step (for
    example because its eventOccurred method returns RESET or
    RESET_DERIVATIVES for example), the stepAccepted method of the pending
    events later in the step were not called. This implied that in the next
    step, these events were still referring to data from previous step, they
    had lost synchronization with the integrator.
    
    JIRA: MATH-695
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1189086 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math/ode/AbstractIntegrator.java
+++ src/main/java/org/apache/commons/math/ode/AbstractIntegrator.java
@@ -40,7 +40,6 @@ import org.apache.commons.math.ode.sampling.AbstractStepInterpolator;
 import org.apache.commons.math.ode.sampling.StepHandler;
 import org.apache.commons.math.util.FastMath;
 import org.apache.commons.math.util.Incrementor;
-import org.apache.commons.math.util.MathUtils;
 import org.apache.commons.math.util.Precision;
 
 /**
@@ -278,7 +277,6 @@ public abstract class AbstractIntegrator implements FirstOrderIntegrator {
 
             double previousT = interpolator.getGlobalPreviousTime();
             final double currentT = interpolator.getGlobalCurrentTime();
-            resetOccurred = false;
 
             // initialize the events states if needed
             if (! statesInitialized) {
@@ -332,6 +330,9 @@ public abstract class AbstractIntegrator implements FirstOrderIntegrator {
                 if (isLastStep) {
                     // the event asked to stop integration
                     System.arraycopy(eventY, 0, y, 0, y.length);
+                    for (final EventState remaining : occuringEvents) {
+                        remaining.stepAccepted(eventT, eventY);
+                    }
                     return eventT;
                 }
 
@@ -341,6 +342,9 @@ public abstract class AbstractIntegrator implements FirstOrderIntegrator {
                     System.arraycopy(eventY, 0, y, 0, y.length);
                     computeDerivatives(eventT, y, yDot);
                     resetOccurred = true;
+                    for (final EventState remaining : occuringEvents) {
+                        remaining.stepAccepted(eventT, eventY);
+                    }
                     return eventT;
                 }
 
