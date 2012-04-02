b9ca51f09f38e5a4d73bcdafd09080c91670379c
Thomas Neidhart <tn@apache.org>
2012-04-02 18:46:42 +0000

    [MATH-776] Use same range check in ctor as in setter for ElitisticListPopulation. Thanks to Reid Hochstedler
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1308454 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/genetics/ElitisticListPopulation.java
+++ src/main/java/org/apache/commons/math3/genetics/ElitisticListPopulation.java
@@ -24,7 +24,7 @@ import org.apache.commons.math3.exception.util.LocalizedFormats;
 import org.apache.commons.math3.util.FastMath;
 
 /**
- * Population of chromosomes which uses elitism (certain percentace of the best
+ * Population of chromosomes which uses elitism (certain percentage of the best
  * chromosomes is directly copied to the next generation).
  *
  * @version $Id$
@@ -42,12 +42,13 @@ public class ElitisticListPopulation extends ListPopulation {
      * @param populationLimit maximal size of the population
      * @param elitismRate how many best chromosomes will be directly transferred to the
      *                    next generation [in %]
+     * @throws OutOfRangeException if the elitism rate is outside the [0, 1] range
      */
     public ElitisticListPopulation(final List<Chromosome> chromosomes,
                                    final int populationLimit,
                                    final double elitismRate) {
         super(chromosomes, populationLimit);
-        this.elitismRate = elitismRate;
+        setElitismRate(elitismRate);
     }
 
     /**
@@ -57,10 +58,11 @@ public class ElitisticListPopulation extends ListPopulation {
      * @param populationLimit maximal size of the population
      * @param elitismRate how many best chromosomes will be directly transferred to the
      *                    next generation [in %]
+     * @throws OutOfRangeException if the elitism rate is outside the [0, 1] range
      */
     public ElitisticListPopulation(final int populationLimit, final double elitismRate) {
         super(populationLimit);
-        this.elitismRate = elitismRate;
+        setElitismRate(elitismRate);
     }
 
     /**
