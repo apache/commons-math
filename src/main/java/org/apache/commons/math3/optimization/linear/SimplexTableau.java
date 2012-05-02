3c4cb189cc91c5ca78cab3ba95dc82bee715d39a
Thomas Neidhart <tn@apache.org>
2012-05-02 18:28:37 +0000

    [MATH-781] use epsilon criteria when deciding to drop columns after phase 1.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1333146 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/optimization/linear/SimplexTableau.java
+++ src/main/java/org/apache/commons/math3/optimization/linear/SimplexTableau.java
@@ -335,7 +335,7 @@ class SimplexTableau implements Serializable {
         // positive cost non-artificial variables
         for (int i = getNumObjectiveFunctions(); i < getArtificialVariableOffset(); i++) {
             final double entry = tableau.getEntry(0, i);
-            if (Precision.compareTo(entry, 0d, maxUlps) > 0) {
+            if (Precision.compareTo(entry, 0d, epsilon) > 0) {
                 columnsToDrop.add(i);
             }
         }
