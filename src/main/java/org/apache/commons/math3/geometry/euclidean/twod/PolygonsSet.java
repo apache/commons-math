dd6cefb0ccb596d0597e2a9991766e109153ab25
Luc Maisonobe <luc@apache.org>
2012-05-13 15:51:56 +0000

    Fixed a wrong assumption on BSP tree attributes.
    The assumption was that the attribute was always a boolean when boundary
    was empty. This assumption was false when emptiness resulted from a
    collapsed boundary. Boolean attributes occur only at leaf nodes, so it
    is the cut sub-hyperplane that needed to be checked.
    
    JIRA: MATH-780
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1337929 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/geometry/euclidean/twod/PolygonsSet.java
+++ src/main/java/org/apache/commons/math3/geometry/euclidean/twod/PolygonsSet.java
@@ -132,7 +132,9 @@ public class PolygonsSet extends AbstractRegion<Euclidean2D, Euclidean1D> {
         final Vector2D[][] v = getVertices();
 
         if (v.length == 0) {
-            if ((Boolean) getTree(false).getAttribute()) {
+            final BSPTree<Euclidean2D> tree = getTree(false);
+            if (tree.getCut() == null && (Boolean) tree.getAttribute()) {
+                // the instance covers the whole space
                 setSize(Double.POSITIVE_INFINITY);
                 setBarycenter(Vector2D.NaN);
             } else {
