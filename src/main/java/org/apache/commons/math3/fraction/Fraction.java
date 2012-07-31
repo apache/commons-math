63a48705a496bf2506121dcddbd8cac2a78f877c
Gilles Sadowski <erans@apache.org>
2012-07-31 14:58:01 +0000

    MATH-835 Avoid overflow.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1367593 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/fraction/Fraction.java
+++ src/main/java/org/apache/commons/math3/fraction/Fraction.java
@@ -594,7 +594,7 @@ public class Fraction
      * @return the fraction percentage as a <tt>double</tt>.
      */
     public double percentageValue() {
-        return multiply(100).doubleValue();
+        return 100 * doubleValue();
     }
 
     /**
--- src/test/java/org/apache/commons/math3/fraction/FractionTest.java
+++ src/test/java/org/apache/commons/math3/fraction/FractionTest.java
@@ -243,6 +243,17 @@ public class FractionTest {
     }
 
     @Test
+    public void testMath835() {
+        final int numer = Integer.MAX_VALUE / 99;
+        final int denom = 1;
+        final double percentage = 100 * ((double) numer) / denom;
+        final Fraction frac = new Fraction(numer, denom);
+        // With the implementation that preceded the fix suggested in MATH-835,
+        // this test was failing, due to overflow.
+        Assert.assertEquals(percentage, frac.percentageValue(), Math.ulp(percentage));
+    }
+
+    @Test
     public void testReciprocal() {
         Fraction f = null;
 
