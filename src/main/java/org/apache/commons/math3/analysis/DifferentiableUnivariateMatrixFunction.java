6fe3ae0e6c4a78938213a6ebbf1080248e81cf70
Luc Maisonobe <luc@apache.org>
2012-09-12 08:55:32 +0000

    Deprecated DifferentiableUnivariateMatrixFunction interface.
    This interface is in fact never used in the library...
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1383854 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/analysis/DifferentiableUnivariateMatrixFunction.java
+++ src/main/java/org/apache/commons/math3/analysis/DifferentiableUnivariateMatrixFunction.java
@@ -21,6 +21,7 @@ package org.apache.commons.math3.analysis;
  *
  * @version $Id$
  * @since 2.0
+ * @deprecated as of 3.1 replaced by  {@link org.apache.commons.math3.analysis.differentiation.UnivariateDifferentiableMatrixFunction}
  */
 public interface DifferentiableUnivariateMatrixFunction
     extends UnivariateMatrixFunction {
