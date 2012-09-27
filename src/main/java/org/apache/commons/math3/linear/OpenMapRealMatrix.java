3a3fec3bb2c7b5a65caac8f8f25e38d3fedcbaba
Sebastien Brisard <celestin@apache.org>
2012-09-27 03:52:34 +0000

    MATH-870: deprecated SparseRealMatrix and OpenMapRealMatrix.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1390831 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/linear/OpenMapRealMatrix.java
+++ src/main/java/org/apache/commons/math3/linear/OpenMapRealMatrix.java
@@ -27,7 +27,13 @@ import org.apache.commons.math3.util.OpenIntToDoubleHashMap;
  *
  * @version $Id$
  * @since 2.0
+ * @deprecated As of version 3.1, this class is deprecated, for reasons exposed
+ * in this JIRA
+ * <a href="https://issues.apache.org/jira/browse/MATH-870">ticket</a>. This
+ * class will be removed in version 4.0.
+ *
  */
+@Deprecated
 public class OpenMapRealMatrix extends AbstractRealMatrix
     implements SparseRealMatrix, Serializable {
     /** Serializable version identifier. */
--- src/main/java/org/apache/commons/math3/linear/SparseRealMatrix.java
+++ src/main/java/org/apache/commons/math3/linear/SparseRealMatrix.java
@@ -22,8 +22,13 @@ package org.apache.commons.math3.linear;
  *
  * @version $Id$
  * @since 2.0
+ * @deprecated As of version 3.1, this class is deprecated, for reasons exposed
+ * in this JIRA
+ * <a href="https://issues.apache.org/jira/browse/MATH-870">ticket</a>. This
+ * class will be removed in version 4.0.
  *
  */
+@Deprecated
 public interface SparseRealMatrix extends RealMatrix {
 
 }
