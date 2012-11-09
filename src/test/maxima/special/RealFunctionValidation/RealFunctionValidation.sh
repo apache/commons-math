e54d3237a37420c6348635a2800625f17fbeeced
Sebastien Brisard <celestin@apache.org>
2012-11-09 08:47:38 +0000

    Typo.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1407382 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/maxima/special/RealFunctionValidation/RealFunctionValidation.sh
+++ src/test/maxima/special/RealFunctionValidation/RealFunctionValidation.sh
@@ -6,4 +6,4 @@ CM3_JAR=$HOME/.m2/repository/org/apache/commons/commons-math3/3.1-SNAPSHOT/commo
 # Location of file RealFunctionValidation.jar
 APP_JAR=$HOME/Documents/workspace/commons-math3/src/test/maxima/special/RealFunctionValidation/RealFunctionValidation.jar
 
-java -cp $CM3_JAR:$APP_JAR RealFunctionValidation logGamma.properties
+java -cp $CM3_JAR:$APP_JAR RealFunctionValidation
