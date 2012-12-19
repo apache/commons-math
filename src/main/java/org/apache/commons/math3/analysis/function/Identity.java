a5f02be0357eac46715e31de654967c723382a46
Luc Maisonobe <luc@apache.org>
2012-12-19 20:32:50 +0000

    removed unused import statements
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1424087 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/analysis/function/Identity.java
+++ src/main/java/org/apache/commons/math3/analysis/function/Identity.java
@@ -18,8 +18,6 @@
 package org.apache.commons.math3.analysis.function;
 
 import org.apache.commons.math3.analysis.DifferentiableUnivariateFunction;
-import org.apache.commons.math3.analysis.FunctionUtils;
-import org.apache.commons.math3.analysis.UnivariateFunction;
 import org.apache.commons.math3.analysis.differentiation.DerivativeStructure;
 import org.apache.commons.math3.analysis.differentiation.UnivariateDifferentiableFunction;
 
