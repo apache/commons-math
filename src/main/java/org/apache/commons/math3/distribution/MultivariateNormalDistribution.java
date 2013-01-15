cedf0d27f9e9341a9e9fa8a192735a0c2e11be40
Gilles Sadowski <erans@apache.org>
2013-01-15 12:15:50 +0000

    MATH-929 Fixed truncated value. Thanks to Piotr Wydrych. Added unit test: comparing density values with univariate normal distribution.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1433367 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/distribution/MultivariateNormalDistribution.java
+++ src/main/java/org/apache/commons/math3/distribution/MultivariateNormalDistribution.java
@@ -180,7 +180,7 @@ public class MultivariateNormalDistribution
             throw new DimensionMismatchException(vals.length, dim);
         }
 
-        return FastMath.pow(2 * FastMath.PI, -dim / 2) *
+        return FastMath.pow(2 * FastMath.PI, -0.5 * dim) *
             FastMath.pow(covarianceMatrixDeterminant, -0.5) *
             getExponentTerm(vals);
     }
