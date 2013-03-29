c72ea892f6da50d49582a0664ef05e7338cecb5d
Phil Steitz <psteitz@apache.org>
2013-03-29 14:36:19 +0000

    Fixed javadoc typos.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1462485 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/analysis/MultivariateVectorFunction.java
+++ src/main/java/org/apache/commons/math3/analysis/MultivariateVectorFunction.java
@@ -28,7 +28,7 @@ public interface MultivariateVectorFunction {
      * Compute the value for the function at the given point.
      * @param point point at which the function must be evaluated
      * @return function value for the given point
-     * @exception IllegalArgumentException if points dimension is wrong
+     * @exception IllegalArgumentException if point's dimension is wrong
      */
     double[] value(double[] point)
         throws IllegalArgumentException;
