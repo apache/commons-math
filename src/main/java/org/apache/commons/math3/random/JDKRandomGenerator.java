a846ced6e3ec8b6b285de2b40aa760699f24571c
Gilles Sadowski <erans@apache.org>
2013-08-01 13:55:22 +0000

    Removed duplicate code.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1509236 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/random/JDKRandomGenerator.java
+++ src/main/java/org/apache/commons/math3/random/JDKRandomGenerator.java
@@ -37,14 +37,6 @@ public class JDKRandomGenerator extends Random implements RandomGenerator {
 
     /** {@inheritDoc} */
     public void setSeed(int[] seed) {
-        // the following number is the largest prime that fits in 32 bits (it is 2^32 - 5)
-        final long prime = 4294967291l;
-
-        long combined = 0l;
-        for (int s : seed) {
-            combined = combined * prime + s;
-        }
-        setSeed(combined);
+        setSeed(RandomGeneratorFactory.convertToLong(seed));
     }
-
 }
