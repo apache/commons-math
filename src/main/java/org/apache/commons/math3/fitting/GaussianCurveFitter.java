354d5520c95c98cb5bbdc485182d452a504bfc5b
Gilles Sadowski <erans@apache.org>
2013-08-23 15:45:49 +0000

    MATH-1014 Javadoc.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1516902 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/fitting/GaussianCurveFitter.java
+++ src/main/java/org/apache/commons/math3/fitting/GaussianCurveFitter.java
@@ -51,20 +51,20 @@ import org.apache.commons.math3.util.FastMath;
  * <p>
  * Usage example:
  * <pre>
- *   GaussianCurveFitter fitter = GaussianCurveFitter.create();
- *   fitter.add(4.0254623,  531026.0);
- *   fitter.add(4.03128248, 984167.0);
- *   fitter.add(4.03839603, 1887233.0);
- *   fitter.add(4.04421621, 2687152.0);
- *   fitter.add(4.05132976, 3461228.0);
- *   fitter.add(4.05326982, 3580526.0);
- *   fitter.add(4.05779662, 3439750.0);
- *   fitter.add(4.0636168,  2877648.0);
- *   fitter.add(4.06943698, 2175960.0);
- *   fitter.add(4.07525716, 1447024.0);
- *   fitter.add(4.08237071, 717104.0);
- *   fitter.add(4.08366408, 620014.0);
- *   double[] parameters = fitter.fit();
+ *   WeightedObservedPoints obs = new WeightedObservedPoints();
+ *   obs.add(4.0254623,  531026.0);
+ *   obs.add(4.03128248, 984167.0);
+ *   obs.add(4.03839603, 1887233.0);
+ *   obs.add(4.04421621, 2687152.0);
+ *   obs.add(4.05132976, 3461228.0);
+ *   obs.add(4.05326982, 3580526.0);
+ *   obs.add(4.05779662, 3439750.0);
+ *   obs.add(4.0636168,  2877648.0);
+ *   obs.add(4.06943698, 2175960.0);
+ *   obs.add(4.07525716, 1447024.0);
+ *   obs.add(4.08237071, 717104.0);
+ *   obs.add(4.08366408, 620014.0);
+ *   double[] parameters = GaussianCurveFitter.create().fit(obs);
  * </pre>
  *
  * @version $Id$
