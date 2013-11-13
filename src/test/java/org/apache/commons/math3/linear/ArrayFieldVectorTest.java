1e1365c7b48da04db1e29f225ab1fabe40d13c5e
Phil Steitz <psteitz@apache.org>
2013-11-13 02:04:32 +0000

    Replaced deprecated method.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1541354 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/math3/linear/ArrayFieldVectorTest.java
+++ src/test/java/org/apache/commons/math3/linear/ArrayFieldVectorTest.java
@@ -249,7 +249,7 @@ public class ArrayFieldVectorTest {
         }
 
         public T[] toArray() {
-            throw unsupported();
+            return data.clone();
         }
 
     }
@@ -429,57 +429,57 @@ public class ArrayFieldVectorTest {
         //octave =  v1 .+ 2.0
         FieldVector<Fraction> v_mapAdd = v1.mapAdd(new Fraction(2));
         Fraction[] result_mapAdd = {new Fraction(3), new Fraction(4), new Fraction(5)};
-        checkArray("compare vectors" ,result_mapAdd,v_mapAdd.getData());
+        checkArray("compare vectors" ,result_mapAdd,v_mapAdd.toArray());
 
         //octave =  v1 .+ 2.0
         FieldVector<Fraction> v_mapAddToSelf = v1.copy();
         v_mapAddToSelf.mapAddToSelf(new Fraction(2));
         Fraction[] result_mapAddToSelf = {new Fraction(3), new Fraction(4), new Fraction(5)};
-        checkArray("compare vectors" ,result_mapAddToSelf,v_mapAddToSelf.getData());
+        checkArray("compare vectors" ,result_mapAddToSelf,v_mapAddToSelf.toArray());
 
         //octave =  v1 .- 2.0
         FieldVector<Fraction> v_mapSubtract = v1.mapSubtract(new Fraction(2));
         Fraction[] result_mapSubtract = {new Fraction(-1), new Fraction(0), new Fraction(1)};
-        checkArray("compare vectors" ,result_mapSubtract,v_mapSubtract.getData());
+        checkArray("compare vectors" ,result_mapSubtract,v_mapSubtract.toArray());
 
         //octave =  v1 .- 2.0
         FieldVector<Fraction> v_mapSubtractToSelf = v1.copy();
         v_mapSubtractToSelf.mapSubtractToSelf(new Fraction(2));
         Fraction[] result_mapSubtractToSelf = {new Fraction(-1), new Fraction(0), new Fraction(1)};
-        checkArray("compare vectors" ,result_mapSubtractToSelf,v_mapSubtractToSelf.getData());
+        checkArray("compare vectors" ,result_mapSubtractToSelf,v_mapSubtractToSelf.toArray());
 
         //octave =  v1 .* 2.0
         FieldVector<Fraction> v_mapMultiply = v1.mapMultiply(new Fraction(2));
         Fraction[] result_mapMultiply = {new Fraction(2), new Fraction(4), new Fraction(6)};
-        checkArray("compare vectors" ,result_mapMultiply,v_mapMultiply.getData());
+        checkArray("compare vectors" ,result_mapMultiply,v_mapMultiply.toArray());
 
         //octave =  v1 .* 2.0
         FieldVector<Fraction> v_mapMultiplyToSelf = v1.copy();
         v_mapMultiplyToSelf.mapMultiplyToSelf(new Fraction(2));
         Fraction[] result_mapMultiplyToSelf = {new Fraction(2), new Fraction(4), new Fraction(6)};
-        checkArray("compare vectors" ,result_mapMultiplyToSelf,v_mapMultiplyToSelf.getData());
+        checkArray("compare vectors" ,result_mapMultiplyToSelf,v_mapMultiplyToSelf.toArray());
 
         //octave =  v1 ./ 2.0
         FieldVector<Fraction> v_mapDivide = v1.mapDivide(new Fraction(2));
         Fraction[] result_mapDivide = {new Fraction(1, 2), new Fraction(1), new Fraction(3, 2)};
-        checkArray("compare vectors" ,result_mapDivide,v_mapDivide.getData());
+        checkArray("compare vectors" ,result_mapDivide,v_mapDivide.toArray());
 
         //octave =  v1 ./ 2.0
         FieldVector<Fraction> v_mapDivideToSelf = v1.copy();
         v_mapDivideToSelf.mapDivideToSelf(new Fraction(2));
         Fraction[] result_mapDivideToSelf = {new Fraction(1, 2), new Fraction(1), new Fraction(3, 2)};
-        checkArray("compare vectors" ,result_mapDivideToSelf,v_mapDivideToSelf.getData());
+        checkArray("compare vectors" ,result_mapDivideToSelf,v_mapDivideToSelf.toArray());
 
         //octave =  v1 .^-1
         FieldVector<Fraction> v_mapInv = v1.mapInv();
         Fraction[] result_mapInv = {new Fraction(1),new Fraction(1, 2),new Fraction(1, 3)};
-        checkArray("compare vectors" ,result_mapInv,v_mapInv.getData());
+        checkArray("compare vectors" ,result_mapInv,v_mapInv.toArray());
 
         //octave =  v1 .^-1
         FieldVector<Fraction> v_mapInvToSelf = v1.copy();
         v_mapInvToSelf.mapInvToSelf();
         Fraction[] result_mapInvToSelf = {new Fraction(1),new Fraction(1, 2),new Fraction(1, 3)};
-        checkArray("compare vectors" ,result_mapInvToSelf,v_mapInvToSelf.getData());
+        checkArray("compare vectors" ,result_mapInvToSelf,v_mapInvToSelf.toArray());
 
     }
 
@@ -499,7 +499,7 @@ public class ArrayFieldVectorTest {
         FieldVectorTestImpl<Fraction> vt2 = new FieldVectorTestImpl<Fraction>(vec2);
         FieldVector<Fraction> v_add_i = v1.add(vt2);
         Fraction[] result_add_i = {new Fraction(5), new Fraction(7), new Fraction(9)};
-        checkArray("compare vect" ,v_add_i.getData(),result_add_i);
+        checkArray("compare vect" ,v_add_i.toArray(),result_add_i);
 
         //octave =  v1 - v2
         ArrayFieldVector<Fraction> v_subtract = v1.subtract(v2);
@@ -508,7 +508,7 @@ public class ArrayFieldVectorTest {
 
         FieldVector<Fraction> v_subtract_i = v1.subtract(vt2);
         Fraction[] result_subtract_i = {new Fraction(-3), new Fraction(-3), new Fraction(-3)};
-        checkArray("compare vect" ,v_subtract_i.getData(),result_subtract_i);
+        checkArray("compare vect" ,v_subtract_i.toArray(),result_subtract_i);
 
         // octave v1 .* v2
         ArrayFieldVector<Fraction>  v_ebeMultiply = v1.ebeMultiply(v2);
@@ -517,7 +517,7 @@ public class ArrayFieldVectorTest {
 
         FieldVector<Fraction>  v_ebeMultiply_2 = v1.ebeMultiply(v2_t);
         Fraction[] result_ebeMultiply_2 = {new Fraction(4), new Fraction(10), new Fraction(18)};
-        checkArray("compare vect" ,v_ebeMultiply_2.getData(),result_ebeMultiply_2);
+        checkArray("compare vect" ,v_ebeMultiply_2.toArray(),result_ebeMultiply_2);
 
         // octave v1 ./ v2
         ArrayFieldVector<Fraction>  v_ebeDivide = v1.ebeDivide(v2);
@@ -526,7 +526,7 @@ public class ArrayFieldVectorTest {
 
         FieldVector<Fraction>  v_ebeDivide_2 = v1.ebeDivide(v2_t);
         Fraction[] result_ebeDivide_2 = {new Fraction(1, 4), new Fraction(2, 5), new Fraction(1, 2)};
-        checkArray("compare vect" ,v_ebeDivide_2.getData(),result_ebeDivide_2);
+        checkArray("compare vect" ,v_ebeDivide_2.toArray(),result_ebeDivide_2);
 
         // octave  dot(v1,v2)
         Fraction dot =  v1.dotProduct(v2);
@@ -548,7 +548,7 @@ public class ArrayFieldVectorTest {
 
         FieldVector<Fraction> v_projection_2 = v1.projection(v2_t);
         Fraction[] result_projection_2 = {new Fraction(128, 77), new Fraction(160, 77), new Fraction(192, 77)};
-        checkArray("compare vect", v_projection_2.getData(), result_projection_2);
+        checkArray("compare vect", v_projection_2.toArray(), result_projection_2);
 
     }
 
