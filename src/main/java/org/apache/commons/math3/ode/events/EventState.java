2c9438da238f61f9e952d301cf22fbe70c5a9405
Luc Maisonobe <luc@apache.org>
2014-01-15 16:48:25 +0000

    Fixed an issue with noisy functions for ODE events detection.
    The issue was not detected in Apache Commons Math but in similar code in
    Orekit (see https://www.orekit.org/forge/projects/orekit/issues/160).
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1558462 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/ode/events/EventState.java
+++ src/main/java/org/apache/commons/math3/ode/events/EventState.java
@@ -290,9 +290,13 @@ public class EventState {
                         (FastMath.abs(root - ta) <= convergence) &&
                         (FastMath.abs(root - previousEventTime) <= convergence)) {
                         // we have either found nothing or found (again ?) a past event,
-                        // retry the substep excluding this value
-                        ta = forward ? ta + convergence : ta - convergence;
-                        ga = f.value(ta);
+                        // retry the substep excluding this value, and taking care to have the
+                        // required sign in case the g function is noisy around its zero and
+                        // crosses the axis several times
+                        do {
+                            ta = forward ? ta + convergence : ta - convergence;
+                            ga = f.value(ta);
+                        } while ((g0Positive ^ (ga >= 0)) && (forward ^ (ta >= tb)));
                         --i;
                     } else if (Double.isNaN(previousEventTime) ||
                                (FastMath.abs(previousEventTime - root) > convergence)) {
