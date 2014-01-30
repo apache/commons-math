8025900b3ac50d2923aa8e52515dd5e55c4c3ad3
Luc Maisonobe <luc@apache.org>
2014-01-30 09:37:08 +0000

    Removed redundant throws
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1562755 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/math3/linear/ConjugateGradient.java
+++ src/main/java/org/apache/commons/math3/linear/ConjugateGradient.java
@@ -151,7 +151,7 @@ public class ConjugateGradient
                                    final RealVector x0)
         throws NullArgumentException, NonPositiveDefiniteOperatorException,
         NonSquareOperatorException, DimensionMismatchException,
-        MaxCountExceededException, NonPositiveDefiniteOperatorException {
+        MaxCountExceededException {
         checkParameters(a, m, b, x0);
         final IterationManager manager = getIterationManager();
         // Initialization of default stopping criterion
