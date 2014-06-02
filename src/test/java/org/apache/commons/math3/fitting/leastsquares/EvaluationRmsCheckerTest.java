326beb22c6aa872cbc0ad3706cfbe706bf9eab41
Evan Ward <evanward@apache.org>
2014-06-02 21:19:09 +0000

    Add EvaluationRmsChecker
    Convergence checker for Evaluations that decides based on change in RMS.
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/math/trunk@1599359 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/math3/fitting/leastsquares/EvaluationRmsCheckerTest.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.math3.fitting.leastsquares;
+
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresProblem.Evaluation;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.optim.ConvergenceChecker;
+import org.junit.Assert;
+import org.junit.Test;
+
+/** Unit tests for {@link EvaluationRmsChecker}. */
+public class EvaluationRmsCheckerTest {
+
+    /** check {@link EvaluationRmsChecker#converged(int, Evaluation, Evaluation)}. */
+    @Test
+    public void testConverged() {
+        //setup
+        ConvergenceChecker<Evaluation> checker = new EvaluationRmsChecker(0.1, 1);
+        Evaluation e200 = mockEvaluation(200);
+        Evaluation e1 = mockEvaluation(1);
+
+        //action + verify
+        //just matches rel tol
+        Assert.assertEquals(true, checker.converged(0, e200, mockEvaluation(210)));
+        //just matches abs tol
+        Assert.assertEquals(true, checker.converged(0, e1, mockEvaluation(1.9)));
+        //matches both
+        Assert.assertEquals(true, checker.converged(0, e1, mockEvaluation(1.01)));
+        //matches neither
+        Assert.assertEquals(false, checker.converged(0, e200, mockEvaluation(300)));
+    }
+
+    /**
+     * Create a mock {@link Evaluation}.
+     *
+     * @param rms the evaluation's rms.
+     * @return a new mock evaluation.
+     */
+    private static Evaluation mockEvaluation(final double rms) {
+        return new Evaluation() {
+            public RealMatrix getCovariances(double threshold) {
+                return null;
+            }
+
+            public RealVector getSigma(double covarianceSingularityThreshold) {
+                return null;
+            }
+
+            public double getRMS() {
+                return rms;
+            }
+
+            public RealMatrix getJacobian() {
+                return null;
+            }
+
+            public double getCost() {
+                return 0;
+            }
+
+            public RealVector getResiduals() {
+                return null;
+            }
+
+            public RealVector getPoint() {
+                return null;
+            }
+        };
+    }
+
+}
