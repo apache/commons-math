575503b13167845bdc29fe40f8e764b9dee5118a
Phil Steitz <phil.steitz@gmail.com>
2014-10-15 15:54:27 -0700

    Fixed javadoc typo.

--- src/main/java/org/apache/commons/math3/exception/MathIllegalStateException.java
+++ src/main/java/org/apache/commons/math3/exception/MathIllegalStateException.java
@@ -24,7 +24,7 @@ import org.apache.commons.math3.exception.util.ExceptionContextProvider;
 /**
  * Base class for all exceptions that signal that the process
  * throwing the exception is in a state that does not comply with
- * the set of states the it is designed to be in.
+ * the set of states that it is designed to be in.
  *
  * @since 2.2
  */
