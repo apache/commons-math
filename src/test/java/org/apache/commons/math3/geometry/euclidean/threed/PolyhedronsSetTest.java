9744a812bdca34cdfc3663e4fbcd40df73def460
Luc Maisonobe <luc@apache.org>
2015-04-09 22:13:45 +0200

    Added dump/parse utilities for BSP trees.
    These utilities are for test and debug purposes only.

--- src/test/java/org/apache/commons/math3/geometry/euclidean/threed/PolyhedronsSetTest.java
+++ src/test/java/org/apache/commons/math3/geometry/euclidean/threed/PolyhedronsSetTest.java
@@ -16,7 +16,11 @@
  */
 package org.apache.commons.math3.geometry.euclidean.threed;
 
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.commons.math3.exception.MathArithmeticException;
 import org.apache.commons.math3.exception.MathIllegalArgumentException;
@@ -29,7 +33,9 @@ import org.apache.commons.math3.geometry.partitioning.BSPTree;
 import org.apache.commons.math3.geometry.partitioning.BSPTreeVisitor;
 import org.apache.commons.math3.geometry.partitioning.BoundaryAttribute;
 import org.apache.commons.math3.geometry.partitioning.Region;
+import org.apache.commons.math3.geometry.partitioning.RegionDumper;
 import org.apache.commons.math3.geometry.partitioning.RegionFactory;
+import org.apache.commons.math3.geometry.partitioning.RegionParser;
 import org.apache.commons.math3.geometry.partitioning.SubHyperplane;
 import org.apache.commons.math3.util.FastMath;
 import org.junit.Assert;
@@ -305,6 +311,57 @@ public class PolyhedronsSetTest {
         } catch (NullPointerException npe) {
             // this is expected
         }
+    }
+
+    @Test
+    public void testDumpParse() throws IOException, ParseException {
+        double tol=1e-8;
+        
+            Vector3D[] verts=new Vector3D[8];
+            double xmin=-1,xmax=1;
+            double ymin=-1,ymax=1;
+            double zmin=-1,zmax=1;
+            verts[0]=new Vector3D(xmin,ymin,zmin);
+            verts[1]=new Vector3D(xmax,ymin,zmin);
+            verts[2]=new Vector3D(xmax,ymax,zmin);
+            verts[3]=new Vector3D(xmin,ymax,zmin);
+            verts[4]=new Vector3D(xmin,ymin,zmax);
+            verts[5]=new Vector3D(xmax,ymin,zmax);
+            verts[6]=new Vector3D(xmax,ymax,zmax);
+            verts[7]=new Vector3D(xmin,ymax,zmax);
+            //
+            int[][] faces=new int[12][];
+            faces[0]=new int[]{3,1,0};  // bottom (-z)
+            faces[1]=new int[]{1,3,2};  // bottom (-z)
+            faces[2]=new int[]{5,7,4};  // top (+z)
+            faces[3]=new int[]{7,5,6};  // top (+z)
+            faces[4]=new int[]{2,5,1};  // right (+x)
+            faces[5]=new int[]{5,2,6};  // right (+x)
+            faces[6]=new int[]{4,3,0};  // left (-x)
+            faces[7]=new int[]{3,4,7};  // left (-x)
+            faces[8]=new int[]{4,1,5};  // front (-y)
+            faces[9]=new int[]{1,4,0};  // front (-y)
+            faces[10]=new int[]{3,6,2}; // back (+y)
+            faces[11]=new int[]{6,3,7}; // back (+y)
+            //
+            Set<SubHyperplane<Euclidean3D>> pset=new HashSet<>();
+            for (int f=0; f<faces.length; f++) {
+                int[] vidx=faces[f];
+                Plane p=new Plane(verts[vidx[0]],verts[vidx[1]],verts[vidx[2]],tol);
+                Vector2D p0=p.toSubSpace(verts[vidx[0]]);
+                Vector2D p1=p.toSubSpace(verts[vidx[1]]);
+                Vector2D p2=p.toSubSpace(verts[vidx[2]]);
+                PolygonsSet lset=new PolygonsSet(tol,p0,p1,p2);
+                pset.add(new SubPlane(p,lset));
+            }
+            PolyhedronsSet polyset=new PolyhedronsSet(pset,tol);
+            Assert.assertEquals(8.0, polyset.getSize(), 1.0e-10);
+            Assert.assertEquals(24.0, polyset.getBoundarySize(), 1.0e-10);
+            String dump = RegionDumper.dump(polyset);
+            PolyhedronsSet parsed = RegionParser.parsePolyhedronsSet(dump);
+            Assert.assertEquals(8.0, parsed.getSize(), 1.0e-10);
+            Assert.assertEquals(24.0, parsed.getBoundarySize(), 1.0e-10);
+            Assert.assertTrue(new RegionFactory<Euclidean3D>().difference(polyset, parsed).isEmpty());
     }
 
     private void checkPoints(Region.Location expected, PolyhedronsSet tree, Vector3D[] points) {
