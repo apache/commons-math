b8e4612d484cbc5d1bb36b397b96a791a1a04928
Luc Maisonobe <luc@apache.org>
2015-04-10 16:33:43 +0200

    Added dump/parse utilities for BSP trees.
    These utilities are for test and debug purposes only.

---
+++ src/test/java/org/apache/commons/math4/geometry/partitioning/RegionDumper.java
@@ -0,0 +1,244 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.math4.geometry.partitioning;
+
+import java.util.Formatter;
+import java.util.Locale;
+
+import org.apache.commons.math4.geometry.Space;
+import org.apache.commons.math4.geometry.euclidean.oned.Euclidean1D;
+import org.apache.commons.math4.geometry.euclidean.oned.IntervalsSet;
+import org.apache.commons.math4.geometry.euclidean.oned.OrientedPoint;
+import org.apache.commons.math4.geometry.euclidean.oned.Vector1D;
+import org.apache.commons.math4.geometry.euclidean.threed.Euclidean3D;
+import org.apache.commons.math4.geometry.euclidean.threed.Plane;
+import org.apache.commons.math4.geometry.euclidean.threed.PolyhedronsSet;
+import org.apache.commons.math4.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math4.geometry.euclidean.twod.Euclidean2D;
+import org.apache.commons.math4.geometry.euclidean.twod.Line;
+import org.apache.commons.math4.geometry.euclidean.twod.PolygonsSet;
+import org.apache.commons.math4.geometry.euclidean.twod.Vector2D;
+import org.apache.commons.math4.geometry.spherical.oned.ArcsSet;
+import org.apache.commons.math4.geometry.spherical.oned.LimitAngle;
+import org.apache.commons.math4.geometry.spherical.oned.Sphere1D;
+import org.apache.commons.math4.geometry.spherical.twod.Circle;
+import org.apache.commons.math4.geometry.spherical.twod.Sphere2D;
+import org.apache.commons.math4.geometry.spherical.twod.SphericalPolygonsSet;
+
+/** Class dumping a string representation of an {@link AbstractRegion}.
+ * <p>
+ * This class is intended for tests and debug purposes only.
+ * </p>
+ * @see RegionParser
+ * @since 3.5
+ */
+public class RegionDumper {
+
+    /** Private constructor for a utility class
+     */
+    private RegionDumper() {
+    }
+
+    /** Get a string representation of an {@link ArcsSet}.
+     * @param arcsSet region to dump
+     * @return string representation of the region
+     */
+    public static String dump(final ArcsSet arcsSet) {
+        final TreeDumper<Sphere1D> visitor = new TreeDumper<Sphere1D>("ArcsSet", arcsSet.getTolerance()) {
+
+            /** {@inheritDoc} */
+            @Override
+            protected void formatHyperplane(final Hyperplane<Sphere1D> hyperplane) {
+                final LimitAngle h = (LimitAngle) hyperplane;
+                getFormatter().format("%22.15e %b %22.15e",
+                                      h.getLocation().getAlpha(), h.isDirect(), h.getTolerance());
+            }
+
+        };
+        arcsSet.getTree(false).visit(visitor);
+        return visitor.getDump();
+    }
+
+    /** Get a string representation of a {@link SphericalPolygonsSet}.
+     * @param sphericalPolygonsSet region to dump
+     * @return string representation of the region
+     */
+    public static String dump(final SphericalPolygonsSet sphericalPolygonsSet) {
+        final TreeDumper<Sphere2D> visitor = new TreeDumper<Sphere2D>("SphericalPolygonsSet", sphericalPolygonsSet.getTolerance()) {
+
+            /** {@inheritDoc} */
+            @Override
+            protected void formatHyperplane(final Hyperplane<Sphere2D> hyperplane) {
+                final Circle h = (Circle) hyperplane;
+                getFormatter().format("%22.15e %22.15e %22.15e %22.15e",
+                                      h.getPole().getX(), h.getPole().getY(), h.getPole().getZ(),
+                                      h.getTolerance());
+            }
+
+        };
+        sphericalPolygonsSet.getTree(false).visit(visitor);
+        return visitor.getDump();
+    }
+
+    /** Get a string representation of an {@link IntervalsSet}.
+     * @param intervalsSet region to dump
+     * @return string representation of the region
+     */
+    public static String dump(final IntervalsSet intervalsSet) {
+        final TreeDumper<Euclidean1D> visitor = new TreeDumper<Euclidean1D>("IntervalsSet", intervalsSet.getTolerance()) {
+
+            /** {@inheritDoc} */
+            @Override
+            protected void formatHyperplane(final Hyperplane<Euclidean1D> hyperplane) {
+                final OrientedPoint h = (OrientedPoint) hyperplane;
+                getFormatter().format("%22.15e %b %22.15e",
+                                      h.getLocation().getX(), h.isDirect(), h.getTolerance());
+            }
+
+        };
+        intervalsSet.getTree(false).visit(visitor);
+        return visitor.getDump();
+    }
+
+    /** Get a string representation of a {@link PolygonsSet}.
+     * @param polygonsSet region to dump
+     * @return string representation of the region
+     */
+    public static String dump(final PolygonsSet polygonsSet) {
+        final TreeDumper<Euclidean2D> visitor = new TreeDumper<Euclidean2D>("PolygonsSet", polygonsSet.getTolerance()) {
+
+            /** {@inheritDoc} */
+            @Override
+            protected void formatHyperplane(final Hyperplane<Euclidean2D> hyperplane) {
+                final Line h = (Line) hyperplane;
+                final Vector2D p = h.toSpace(Vector1D.ZERO);
+                getFormatter().format("%22.15e %22.15e %22.15e %22.15e",
+                                      p.getX(), p.getY(), h.getAngle(), h.getTolerance());
+            }
+
+        };
+        polygonsSet.getTree(false).visit(visitor);
+        return visitor.getDump();
+    }
+
+    /** Get a string representation of a {@link PolyhedronsSet}.
+     * @param polyhedronsSet region to dump
+     * @return string representation of the region
+     */
+    public static String dump(final PolyhedronsSet polyhedronsSet) {
+        final TreeDumper<Euclidean3D> visitor = new TreeDumper<Euclidean3D>("PolyhedronsSet", polyhedronsSet.getTolerance()) {
+
+            /** {@inheritDoc} */
+            @Override
+            protected void formatHyperplane(final Hyperplane<Euclidean3D> hyperplane) {
+                final Plane h = (Plane) hyperplane;
+                final Vector3D p = h.toSpace(Vector2D.ZERO);
+                getFormatter().format("%22.15e %22.15e %22.15e %22.15e %22.15e %22.15e %22.15e",
+                                      p.getX(), p.getY(), p.getZ(),
+                                      h.getNormal().getX(), h.getNormal().getY(), h.getNormal().getZ(),
+                                      h.getTolerance());
+            }
+
+        };
+        polyhedronsSet.getTree(false).visit(visitor);
+        return visitor.getDump();
+    }
+
+    /** Dumping visitor.
+     * @param <S> Type of the space.
+     */
+    private abstract static class TreeDumper<S extends Space> implements BSPTreeVisitor<S> {
+
+        /** Builder for the string representation of the dumped tree. */
+        private final StringBuilder dump;
+
+        /** Formatter for strings. */
+        private final Formatter formatter;
+
+        /** Current indentation prefix. */
+        private String prefix;
+
+        /** Simple constructor.
+         * @param type type of the region to dump
+         * @param tolerance tolerance of the region
+         */
+        public TreeDumper(final String type, final double tolerance) {
+            this.dump      = new StringBuilder();
+            this.formatter = new Formatter(dump, Locale.US);
+            this.prefix    = "";
+            formatter.format("%s%n", type);
+            formatter.format("tolerance %22.15e%n", tolerance);
+        }
+
+        /** Get the string representation of the tree.
+         * @return string representation of the tree.
+         */
+        public String getDump() {
+            return dump.toString();
+        }
+
+        /** Get the formatter to use.
+         * @return formatter to use
+         */
+        protected Formatter getFormatter() {
+            return formatter;
+        }
+
+        /** Format a string representation of the hyperplane underlying a cut sub-hyperplane.
+         * @param hyperplane hyperplane to format
+         */
+        protected abstract void formatHyperplane(Hyperplane<S> hyperplane);
+
+        /** {@inheritDoc} */
+        @Override
+        public Order visitOrder(final BSPTree<S> node) {
+            return Order.SUB_MINUS_PLUS;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public void visitInternalNode(final BSPTree<S> node) {
+            formatter.format("%s %s internal ", prefix, type(node));
+            formatHyperplane(node.getCut().getHyperplane());
+            formatter.format("%n");
+            prefix = prefix + "  ";
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public void visitLeafNode(final BSPTree<S> node) {
+            formatter.format("%s %s leaf %s%n",
+                             prefix, type(node), node.getAttribute());
+            for (BSPTree<S> n = node;
+                 n.getParent() != null && n == n.getParent().getPlus();
+                 n = n.getParent()) {
+                prefix = prefix.substring(0, prefix.length() - 2);
+            }
+        }
+
+        /** Get the type of the node.
+         * @param node node to check
+         * @return "plus " or "minus" depending on the node being the plus or minus
+         * child of its parent ("plus " is arbitrarily returned for the root node)
+         */
+        private String type(final BSPTree<S> node) {
+            return (node.getParent() != null && node == node.getParent().getMinus()) ? "minus" : "plus ";
+        }
+
+    }
+
+}
---
+++ src/test/java/org/apache/commons/math4/geometry/partitioning/RegionParser.java
@@ -0,0 +1,303 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.math4.geometry.partitioning;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.StringTokenizer;
+
+import org.apache.commons.math4.geometry.Space;
+import org.apache.commons.math4.geometry.euclidean.oned.Euclidean1D;
+import org.apache.commons.math4.geometry.euclidean.oned.IntervalsSet;
+import org.apache.commons.math4.geometry.euclidean.oned.OrientedPoint;
+import org.apache.commons.math4.geometry.euclidean.oned.Vector1D;
+import org.apache.commons.math4.geometry.euclidean.threed.Euclidean3D;
+import org.apache.commons.math4.geometry.euclidean.threed.Plane;
+import org.apache.commons.math4.geometry.euclidean.threed.PolyhedronsSet;
+import org.apache.commons.math4.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math4.geometry.euclidean.twod.Euclidean2D;
+import org.apache.commons.math4.geometry.euclidean.twod.Line;
+import org.apache.commons.math4.geometry.euclidean.twod.PolygonsSet;
+import org.apache.commons.math4.geometry.euclidean.twod.Vector2D;
+import org.apache.commons.math4.geometry.spherical.oned.ArcsSet;
+import org.apache.commons.math4.geometry.spherical.oned.LimitAngle;
+import org.apache.commons.math4.geometry.spherical.oned.S1Point;
+import org.apache.commons.math4.geometry.spherical.oned.Sphere1D;
+import org.apache.commons.math4.geometry.spherical.twod.Circle;
+import org.apache.commons.math4.geometry.spherical.twod.Sphere2D;
+import org.apache.commons.math4.geometry.spherical.twod.SphericalPolygonsSet;
+
+/** Class parsing a string representation of an {@link AbstractRegion}.
+ * <p>
+ * This class is intended for tests and debug purposes only.
+ * </p>
+ * @see RegionDumper
+ * @since 3.5
+ */
+public class RegionParser {
+
+    /** Private constructor for a utility class
+     */
+    private RegionParser() {
+    }
+
+    /** Parse a string representation of an {@link ArcsSet}.
+     * @param s string to parse
+     * @return parsed region
+     * @exception IOException if the string cannot be read
+     * @exception ParseException if the string cannot be parsed
+     */
+    public static ArcsSet parseArcsSet(final String s)
+        throws IOException, ParseException {
+        final TreeBuilder<Sphere1D> builder = new TreeBuilder<Sphere1D>("ArcsSet", s) {
+
+            /** {@inheritDoc} */
+            @Override
+            protected LimitAngle parseHyperplane()
+                throws IOException, ParseException {
+                return new LimitAngle(new S1Point(getNumber()), getBoolean(), getNumber());
+            }
+
+        };
+        return new ArcsSet(builder.getTree(), builder.getTolerance());
+    }
+
+    /** Parse a string representation of a {@link SphericalPolygonsSet}.
+     * @param s string to parse
+     * @return parsed region
+     * @exception IOException if the string cannot be read
+     * @exception ParseException if the string cannot be parsed
+     */
+    public static SphericalPolygonsSet parseSphericalPolygonsSet(final String s)
+        throws IOException, ParseException {
+        final TreeBuilder<Sphere2D> builder = new TreeBuilder<Sphere2D>("SphericalPolygonsSet", s) {
+
+            /** {@inheritDoc} */
+            @Override
+            public Circle parseHyperplane()
+                throws IOException, ParseException {
+                return new Circle(new Vector3D(getNumber(), getNumber(), getNumber()), getNumber());
+            }
+
+        };
+        return new SphericalPolygonsSet(builder.getTree(), builder.getTolerance());
+    }
+
+    /** Parse a string representation of an {@link IntervalsSet}.
+     * @param s string to parse
+     * @return parsed region
+     * @exception IOException if the string cannot be read
+     * @exception ParseException if the string cannot be parsed
+     */
+    public static IntervalsSet parseIntervalsSet(final String s)
+        throws IOException, ParseException {
+        final TreeBuilder<Euclidean1D> builder = new TreeBuilder<Euclidean1D>("IntervalsSet", s) {
+
+            /** {@inheritDoc} */
+            @Override
+            public OrientedPoint parseHyperplane()
+                throws IOException, ParseException {
+                return new OrientedPoint(new Vector1D(getNumber()), getBoolean(), getNumber());
+            }
+
+        };
+        return new IntervalsSet(builder.getTree(), builder.getTolerance());
+    }
+
+    /** Parse a string representation of a {@link PolygonsSet}.
+     * @param s string to parse
+     * @return parsed region
+     * @exception IOException if the string cannot be read
+     * @exception ParseException if the string cannot be parsed
+     */
+    public static PolygonsSet parsePolygonsSet(final String s)
+        throws IOException, ParseException {
+        final TreeBuilder<Euclidean2D> builder = new TreeBuilder<Euclidean2D>("PolygonsSet", s) {
+
+            /** {@inheritDoc} */
+            @Override
+            public Line parseHyperplane()
+                throws IOException, ParseException {
+                return new Line(new Vector2D(getNumber(), getNumber()), getNumber(), getNumber());
+            }
+
+        };
+        return new PolygonsSet(builder.getTree(), builder.getTolerance());
+    }
+
+    /** Parse a string representation of a {@link PolyhedronsSet}.
+     * @param s string to parse
+     * @return parsed region
+     * @exception IOException if the string cannot be read
+     * @exception ParseException if the string cannot be parsed
+     */
+    public static PolyhedronsSet parsePolyhedronsSet(final String s)
+        throws IOException, ParseException {
+        final TreeBuilder<Euclidean3D> builder = new TreeBuilder<Euclidean3D>("PolyhedronsSet", s) {
+
+            /** {@inheritDoc} */
+            @Override
+            public Plane parseHyperplane()
+                throws IOException, ParseException {
+                return new Plane(new Vector3D(getNumber(), getNumber(), getNumber()),
+                                 new Vector3D(getNumber(), getNumber(), getNumber()),
+                                 getNumber());
+            }
+
+        };
+        return new PolyhedronsSet(builder.getTree(), builder.getTolerance());
+    }
+
+    /** Local class for building an {@link AbstractRegion} tree.
+     * @param <S> Type of the space.
+     */
+    private abstract static class TreeBuilder<S extends Space> {
+
+        /** Keyword for tolerance. */
+        private static final String TOLERANCE = "tolerance";
+
+        /** Keyword for internal nodes. */
+        private static final String INTERNAL  = "internal";
+
+        /** Keyword for leaf nodes. */
+        private static final String LEAF      = "leaf";
+
+        /** Keyword for plus children trees. */
+        private static final String PLUS      = "plus";
+
+        /** Keyword for minus children trees. */
+        private static final String MINUS     = "minus";
+
+        /** Keyword for true flags. */
+        private static final String TRUE      = "true";
+
+        /** Keyword for false flags. */
+        private static final String FALSE     = "false";
+
+        /** Tree root. */
+        private BSPTree<S> root;
+
+        /** Tolerance. */
+        private final double tolerance;
+
+        /** Tokenizer parsing string representation. */
+        private final StringTokenizer tokenizer;
+
+        /** Simple constructor.
+         * @param type type of the expected representation
+         * @param reader reader for the string representation
+         * @exception IOException if the string cannot be read
+         * @exception ParseException if the string cannot be parsed
+         */
+        public TreeBuilder(final String type, final String s)
+            throws IOException, ParseException {
+            root = null;
+            tokenizer = new StringTokenizer(s);
+            getWord(type);
+            getWord(TOLERANCE);
+            tolerance = getNumber();
+            getWord(PLUS);
+            root = new BSPTree<S>();
+            parseTree(root);
+            if (tokenizer.hasMoreTokens()) {
+                throw new ParseException("unexpected " + tokenizer.nextToken(), 0);
+            }
+        }
+
+        /** Parse a tree.
+         * @param node start node
+         * @exception IOException if the string cannot be read
+         * @exception ParseException if the string cannot be parsed
+         */
+        private void parseTree(final BSPTree<S> node)
+            throws IOException, ParseException {
+            if (INTERNAL.equals(getWord(INTERNAL, LEAF))) {
+                // this is an internal node, it has a cut sub-hyperplane (stored as a whole hyperplane)
+                // then a minus tree, then a plus tree
+                node.insertCut(parseHyperplane());
+                getWord(MINUS);
+                parseTree(node.getMinus());
+                getWord(PLUS);
+                parseTree(node.getPlus());
+            } else {
+                // this is a leaf node, it has only an inside/outside flag
+                node.setAttribute(getBoolean());
+            }
+        }
+
+        /** Get next word.
+         * @param allowed allowed values
+         * @return parsed word
+         * @exception IOException if the string cannot be read
+         * @exception ParseException if the string cannot be parsed
+         */
+        protected String getWord(final String ... allowed)
+            throws IOException, ParseException {
+            final String token = tokenizer.nextToken();
+            for (final String a : allowed) {
+                if (a.equals(token)) {
+                    return token;
+                }
+            }
+            throw new ParseException(token + " != " + allowed[0], 0);
+        }
+
+        /** Get next number.
+         * @return parsed number
+         * @exception IOException if the string cannot be read
+         * @exception NumberFormatException if the string cannot be parsed
+         */
+        protected double getNumber()
+            throws IOException, NumberFormatException {
+            return Double.parseDouble(tokenizer.nextToken());
+        }
+
+        /** Get next boolean.
+         * @return parsed boolean
+         * @exception IOException if the string cannot be read
+         * @exception ParseException if the string cannot be parsed
+         */
+        protected boolean getBoolean()
+            throws IOException, ParseException {
+            return getWord(TRUE, FALSE).equals(TRUE);
+        }
+
+        /** Get the built tree.
+         * @return built tree
+         */
+        public BSPTree<S> getTree() {
+            return root;
+        }
+
+        /** Get the tolerance.
+         * @return tolerance
+         */
+        public double getTolerance() {
+            return tolerance;
+        }
+
+        /** Parse an hyperplane.
+         * @return next hyperplane from the stream
+         * @exception IOException if the string cannot be read
+         * @exception ParseException if the string cannot be parsed
+         */
+        protected abstract Hyperplane<S> parseHyperplane()
+            throws IOException, ParseException;
+
+    }
+
+}
