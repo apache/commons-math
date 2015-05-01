321269ed9aa84d15b18296ee6e73d53489efb622
Thomas Neidhart <thomas.neidhart@gmail.com>
2015-05-01 13:24:48 +0200

    [MATH-1220] Improve performance of ZipfDistribution.sample(). Thanks to Otmar Ertl.

--- src/main/java/org/apache/commons/math3/distribution/ZipfDistribution.java
+++ src/main/java/org/apache/commons/math3/distribution/ZipfDistribution.java
@@ -43,6 +43,8 @@ public class ZipfDistribution extends AbstractIntegerDistribution {
     private double numericalVariance = Double.NaN;
     /** Whether or not the numerical variance has been calculated */
     private boolean numericalVarianceIsCalculated = false;
+    /** The sampler to be used for the sample() method */
+    private transient ZipfRejectionSampler sampler;
 
     /**
      * Create a new Zipf distribution with the given number of elements and
@@ -258,5 +260,153 @@ public class ZipfDistribution extends AbstractIntegerDistribution {
     public boolean isSupportConnected() {
         return true;
     }
-}
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * An instrumental distribution g(k) is used to generate random values by
+     * rejection sampling. g(k) is defined as g(1):= 1 and g(k) := I(-s,k-1/2,k+1/2)
+     * for k larger than 1, where s denotes the exponent of the Zipf distribution
+     * and I(r,a,b) is the integral of x^r for x from a to b.
+     * <p>
+     * Since 1^x^s is a convex function, Jensens's inequality gives
+     * I(-s,k-1/2,k+1/2) >= 1/k^s for all positive k and non-negative s.
+     * In order to limit the rejection rate for large exponents s,
+     * the instrumental distribution weight is differently defined for value 1.
+     */
+    @Override
+    public int sample() {
+        if (sampler == null) {
+            sampler = new ZipfRejectionSampler(numberOfElements, exponent);
+        }
+        return sampler.sample(random);
+    }
+
+    /**
+     * Utility class implementing a rejection sampling method for a discrete,
+     * bounded Zipf distribution.
+     *
+     * @since 3.6
+     */
+    static final class ZipfRejectionSampler {
+
+        /** Number of elements. */
+        private final int numberOfElements;
+        /** Exponent parameter of the distribution. */
+        private final double exponent;
+        /** Cached tail weight of instrumental distribution used for rejection sampling */
+        private double instrumentalDistributionTailWeight = Double.NaN;
+
+        ZipfRejectionSampler(final int numberOfElements, final double exponent) {
+            this.numberOfElements = numberOfElements;
+            this.exponent = exponent;
+        }
+
+        int sample(final RandomGenerator random) {
+            if (Double.isNaN(instrumentalDistributionTailWeight)) {
+                instrumentalDistributionTailWeight = integratePowerFunction(-exponent, 1.5, numberOfElements+0.5);
+            }
+
+            while(true) {
+                final double randomValue = random.nextDouble()*(instrumentalDistributionTailWeight + 1.);
+                if (randomValue < instrumentalDistributionTailWeight) {
+                    final double q = randomValue / instrumentalDistributionTailWeight;
+                    final int sample = sampleFromInstrumentalDistributionTail(q);
+                    if (random.nextDouble() < acceptanceRateForTailSample(sample)) {
+                        return sample;
+                    }
+                }
+                else {
+                    return 1;
+                }
+            }
+        }
+
+        /**
+         * Returns a sample from the instrumental distribution tail for a given
+         * uniformly distributed random value.
+         *
+         * @param q a uniformly distributed random value taken from [0,1]
+         * @return a sample in the range [2, {@link #numberOfElements}]
+         */
+        int sampleFromInstrumentalDistributionTail(double q) {
+            final double a = 1.5;
+            final double b = numberOfElements + 0.5;
+            final double logBdviA = FastMath.log(b / a);
+
+            final int result  = (int) (a * FastMath.exp(logBdviA * helper1(q, logBdviA * (1. - exponent))) + 0.5);
+            if (result < 2) {
+                return 2;
+            }
+            if (result > numberOfElements) {
+                return numberOfElements;
+            }
+            return result;
+        }
+
+        /**
+         * Helper function that calculates log((1-q)+q*exp(x))/x.
+         * <p>
+         * A Taylor series expansion is used, if x is close to 0.
+         *
+         * @param q a value in the range [0,1]
+         * @param
+         * @return log((1-q)+q*exp(x))/x
+         */
+        static double helper1(final double q, final double x) {
+            if (Math.abs(x) > 1e-8) {
+                return FastMath.log((1.-q)+q*FastMath.exp(x))/x;
+            }
+            else {
+                return q*(1.+(1./2.)*x*(1.-q)*(1+(1./3.)*x*((1.-2.*q) + (1./4.)*x*(6*q*q*(q-1)+1))));
+            }
+        }
+
+        /**
+         * Helper function to calculate (exp(x)-1)/x.
+         * <p>
+         * A Taylor series expansion is used, if x is close to 0.
+         *
+         * @return (exp(x)-1)/x if x is non-zero, 1 if x=0
+         */
+        static double helper2(final double x) {
+            if (FastMath.abs(x)>1e-8) {
+                return FastMath.expm1(x)/x;
+            }
+            else {
+                return 1.+x*(1./2.)*(1.+x*(1./3.)*(1.+x*(1./4.)));
+            }
+        }
+
+        /**
+         * Integrates the power function x^r from x=a to b.
+         *
+         * @param r the exponent
+         * @param a the integral lower bound
+         * @param b the integral upper bound
+         * @return the calculated integral value
+         */
+        static double integratePowerFunction(final double r, final double a, final double b) {
+            final double logA = FastMath.log(a);
+            final double logBdivA = FastMath.log(b/a);
+            return FastMath.exp((1.+r)*logA)*helper2((1.+r)*logBdivA)*logBdivA;
+
+        }
+
+        /**
+         * Calculates the acceptance rate for a sample taken from the tail of the instrumental distribution.
+         * <p>
+         * The acceptance rate is given by the ratio k^(-s)/I(-s,k-0.5, k+0.5)
+         * where I(r,a,b) is the integral of x^r for x from a to b.
+         *
+         * @param k the value which has been sampled using the instrumental distribution
+         * @return the acceptance rate
+         */
+        double acceptanceRateForTailSample(int k) {
+            final double a = FastMath.log1p(1./(2.*k-1.));
+            final double b = FastMath.log1p(2./(2.*k-1.));
+            return FastMath.exp((1.-exponent)*a)/(k*b*helper2((1.-exponent)*b));
+        }
+    }
+
+}
