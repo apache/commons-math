fbf6259e0fd4fc85ff55ff7a496b40f98cca43f0
Phil Steitz <phil.steitz@gmail.com>
2015-05-13 12:10:33 -0700

    Document NPE.  JIRA: MATH-1224.

--- src/main/java/org/apache/commons/math3/stat/correlation/SpearmansCorrelation.java
+++ src/main/java/org/apache/commons/math3/stat/correlation/SpearmansCorrelation.java
@@ -106,6 +106,7 @@ public class SpearmansCorrelation {
      * Calculate the Spearman Rank Correlation Matrix.
      *
      * @return Spearman Rank Correlation Matrix
+     * @throws NullPointerException if this instance was created with no data
      */
     public RealMatrix getCorrelationMatrix() {
         return rankCorrelation.getCorrelationMatrix();
@@ -120,6 +121,8 @@ public class SpearmansCorrelation {
      * <code>rankTransform(matrix)</code> is the result of applying the
      * configured <code>RankingAlgorithm</code> to each of the columns of
      * <code>matrix.</code>
+     *
+     * <p>Returns null if this instance was created with no data.</p>
      *
      * @return PearsonsCorrelation among ranked column data
      */
