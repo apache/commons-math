175e1b3637b0be2cdc0c4c76b1a924924d2f4c62
Luc Maisonobe <luc@apache.org>
2015-11-03 21:45:53 +0100

    Spurious semicolon.

--- src/main/java/org/apache/commons/math4/geometry/partitioning/BoundaryProjector.java
+++ src/main/java/org/apache/commons/math4/geometry/partitioning/BoundaryProjector.java
@@ -162,7 +162,7 @@ class BoundaryProjector<S extends Space, T extends Space> implements BSPTreeVisi
             if (region != null) {
                 list.add(region);
             }
-        };
+        }
     }
 
     /** Check if a projected point lies on a boundary part.
