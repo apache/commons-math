23b351c89461009a3f772c07edd47a9a2a0b9e7b
Phil Steitz <phil.steitz@gmail.com>
2015-11-22 12:18:46 -0700

    Removed trailing spaces.

--- src/test/R/KolmogorovSmirnovDistributionTestCases.R
+++ src/test/R/KolmogorovSmirnovDistributionTestCases.R
@@ -21,16 +21,16 @@ ps <- c(0.005, 0.02, 0.031111, 0.04)
 for (n in ns) {
   for (p in ps) {
     res <- .C("pkolmogorov2x", p = as.double(p), n = as.integer(n), PACKAGE = "stats")$p
-    
+
     cat("/* formatC(.C(\"pkolmogorov2x\", p = as.double(", p, "), n = as.integer(", n, "), PACKAGE = \"stats\")$p, 40) gives\n", sep = "")
     cat(" * ", formatC(res, digits = 40), "\n", sep = "")
     cat(" */\n")
-    
+
     cat("dist = new KolmogorovSmirnovDistributionImpl(", n, ");\n", sep = "")
     #cat("Assert.assertEquals(", formatC(res, digits = 40), ", dist.cdf(", p, ", true), TOLERANCE);\n", sep = "")
     cat("Assert.assertEquals(", formatC(res, digits = 40), ", dist.cdf(", p, ", false), TOLERANCE);\n", sep = "")
     cat("\n")
-    
+
     #cat("System.out.println(\"", formatC(res, digits = 20), " - \" + dist.cdf(", p, ", false) + \" = \" + (", res, " - dist.cdf(", p, ", false)));\n", sep = "")
   }
 }
--- src/test/R/WeibullDistributionTestCases.R
+++ src/test/R/WeibullDistributionTestCases.R
@@ -40,7 +40,7 @@ verifyDistribution <- function(points, expected, alpha, beta, tol) {
         displayPadded(output, SUCCEEDED, WIDTH)
     } else {
         displayPadded(output, FAILED, WIDTH)
-    }       
+    }
 }
 
 # function to verify density computations
@@ -56,7 +56,7 @@ verifyDensity <- function(points, expected, alpha, beta, tol) {
         displayPadded(output, SUCCEEDED, WIDTH)
     } else {
         displayPadded(output, FAILED, WIDTH)
-    }       
+    }
 }
 
 # function to verify quantiles
@@ -72,7 +72,7 @@ verifyQuantiles <- function(points, expected, alpha, beta, tol) {
         displayPadded(output, SUCCEEDED, WIDTH)
     } else {
         displayPadded(output, FAILED, WIDTH)
-    }    
+    }
 }
 
 #--------------------------------------------------------------------------
@@ -84,7 +84,7 @@ distributionValues <- c(0.001, 0.01, 0.025, 0.05, 0.1, 0.999, 0.990, 0.975, 0.95
 densityValues <- c(0.180535929306, 0.262801138133, 0.301905425199, 0.330899152971, 0.353441418887, 0.000788590320203,
                  0.00737060094841, 0.0177576041516, 0.0343043442574, 0.065664589369)
 distributionPoints <- c(0.00664355180993, 0.0454328283309, 0.0981162737374, 0.176713524579, 0.321946865392,
-                 10.5115496887, 7.4976304671, 6.23205600701, 5.23968436955, 4.20790282578)              
+                 10.5115496887, 7.4976304671, 6.23205600701, 5.23968436955, 4.20790282578)
 verifyQuantiles(distributionValues, distributionPoints, shape, scale, tol)
 verifyDistribution(distributionPoints, distributionValues, shape, scale, tol)
 verifyDensity(distributionPoints, densityValues, shape, scale, tol)
