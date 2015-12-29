fb83ba3fdb46537a36832a900f124d4c400f7ed9
Dave Brosius <dbrosius@mebigfatguy.com>
2015-12-29 13:52:48 -0500

    push down allocations/calculations to where they are needed

--- src/main/java/org/apache/commons/math4/stat/interval/ClopperPearsonInterval.java
+++ src/main/java/org/apache/commons/math4/stat/interval/ClopperPearsonInterval.java
@@ -39,16 +39,16 @@ public class ClopperPearsonInterval implements BinomialConfidenceInterval {
 
         final FDistribution distributionLowerBound = new FDistribution(2 * (numberOfTrials - numberOfSuccesses + 1),
                                                                        2 * numberOfSuccesses);
-        final double fValueLowerBound = distributionLowerBound.inverseCumulativeProbability(1 - alpha);
         if (numberOfSuccesses > 0) {
+            final double fValueLowerBound = distributionLowerBound.inverseCumulativeProbability(1 - alpha);
             lowerBound = numberOfSuccesses /
                          (numberOfSuccesses + (numberOfTrials - numberOfSuccesses + 1) * fValueLowerBound);
         }
 
         final FDistribution distributionUpperBound = new FDistribution(2 * (numberOfSuccesses + 1),
                                                                        2 * (numberOfTrials - numberOfSuccesses));
-        final double fValueUpperBound = distributionUpperBound.inverseCumulativeProbability(1 - alpha);
         if (numberOfSuccesses > 0) {
+            final double fValueUpperBound = distributionUpperBound.inverseCumulativeProbability(1 - alpha);
             upperBound = (numberOfSuccesses + 1) * fValueUpperBound /
                          (numberOfTrials - numberOfSuccesses + (numberOfSuccesses + 1) * fValueUpperBound);
         }
