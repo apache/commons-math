dccc1255413adc1a212dbe3bf40accb70ccd44ef
Gilles <erans@apache.org>
2016-06-11 21:01:45 +0200

    Formatting (tabs, alignment).

--- src/main/java/org/apache/commons/math4/stat/interval/ClopperPearsonInterval.java
+++ src/main/java/org/apache/commons/math4/stat/interval/ClopperPearsonInterval.java
@@ -30,29 +30,29 @@ public class ClopperPearsonInterval implements BinomialConfidenceInterval {
 
     /** {@inheritDoc} */
     @Override
-    public ConfidenceInterval createInterval(int numberOfTrials, int numberOfSuccesses,
+    public ConfidenceInterval createInterval(int numberOfTrials,
+                                             int numberOfSuccesses,
                                              double confidenceLevel) {
         IntervalUtils.checkParameters(numberOfTrials, numberOfSuccesses, confidenceLevel);
         double lowerBound = 0;
         double upperBound = 0;
 
         if (numberOfSuccesses > 0) {
-	    final double alpha = 0.5 * (1 - confidenceLevel);
+            final double alpha = 0.5 * (1 - confidenceLevel);
 
-	    final FDistribution distributionLowerBound = new FDistribution(2 * (numberOfTrials - numberOfSuccesses + 1),
-									   2 * numberOfSuccesses);
+            final FDistribution distributionLowerBound = new FDistribution(2 * (numberOfTrials - numberOfSuccesses + 1),
+                                                                           2 * numberOfSuccesses);
             final double fValueLowerBound = distributionLowerBound.inverseCumulativeProbability(1 - alpha);
             lowerBound = numberOfSuccesses /
-                         (numberOfSuccesses + (numberOfTrials - numberOfSuccesses + 1) * fValueLowerBound);
+                (numberOfSuccesses + (numberOfTrials - numberOfSuccesses + 1) * fValueLowerBound);
 
-	    final FDistribution distributionUpperBound = new FDistribution(2 * (numberOfSuccesses + 1),
-									   2 * (numberOfTrials - numberOfSuccesses));
+            final FDistribution distributionUpperBound = new FDistribution(2 * (numberOfSuccesses + 1),
+                                                                           2 * (numberOfTrials - numberOfSuccesses));
             final double fValueUpperBound = distributionUpperBound.inverseCumulativeProbability(1 - alpha);
             upperBound = (numberOfSuccesses + 1) * fValueUpperBound /
-                         (numberOfTrials - numberOfSuccesses + (numberOfSuccesses + 1) * fValueUpperBound);
+                (numberOfTrials - numberOfSuccesses + (numberOfSuccesses + 1) * fValueUpperBound);
         }
 
         return new ConfidenceInterval(lowerBound, upperBound, confidenceLevel);
     }
-
 }
