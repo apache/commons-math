e7b4025ee0b6152f9bbf7a72a317f977402a27f7
Gilles <erans@apache.org>
2017-05-10 15:27:06 +0200

    Made field "private" (unit test).

--- src/test/java/org/apache/commons/math4/stat/inference/KolmogorovSmirnovTestTest.java
+++ src/test/java/org/apache/commons/math4/stat/inference/KolmogorovSmirnovTestTest.java
@@ -38,8 +38,7 @@ import org.junit.Test;
  * @since 3.3
  */
 public class KolmogorovSmirnovTestTest {
-
-    protected static final double TOLERANCE = 10e-10;
+    private static final double TOLERANCE = 10e-10;
     private static final int MONTE_CARLO_ITERATIONS = 1000000;
     private static final int LARGE_SAMPLE_PRODUCT = 10000;
 
