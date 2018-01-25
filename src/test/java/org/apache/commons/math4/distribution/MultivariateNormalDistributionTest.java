3e24c28e68e617735b25791b0bc3f67f31624ebf
Gilles <erans@apache.org>
2018-01-25 18:35:44 +0100

    MATH-1443: Depend on "Commons Statistics".

--- src/test/java/org/apache/commons/math4/distribution/MultivariateNormalDistributionTest.java
+++ src/test/java/org/apache/commons/math4/distribution/MultivariateNormalDistributionTest.java
@@ -18,7 +18,7 @@
 package org.apache.commons.math4.distribution;
 
 import org.apache.commons.math4.distribution.MultivariateNormalDistribution;
-import org.apache.commons.math4.distribution.NormalDistribution;
+import org.apache.commons.statistics.distribution.NormalDistribution;
 import org.apache.commons.math4.linear.RealMatrix;
 import org.apache.commons.rng.simple.RandomSource;
 import org.apache.commons.math4.stat.correlation.Covariance;
