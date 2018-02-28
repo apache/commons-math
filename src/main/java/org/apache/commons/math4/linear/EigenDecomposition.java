0f25f11ce63fe6d6096a13bb2ac00b8c3f98ef62
Gilles <erans@apache.org>
2018-02-28 02:07:37 +0100

    MATH-1416: Depend on "Commons Numbers".

--- src/main/java/org/apache/commons/math4/linear/EigenDecomposition.java
+++ src/main/java/org/apache/commons/math4/linear/EigenDecomposition.java
@@ -17,14 +17,14 @@
 
 package org.apache.commons.math4.linear;
 
-import org.apache.commons.math4.complex.Complex;
+import org.apache.commons.numbers.complex.Complex;
+import org.apache.commons.numbers.core.Precision;
 import org.apache.commons.math4.exception.DimensionMismatchException;
 import org.apache.commons.math4.exception.MathArithmeticException;
 import org.apache.commons.math4.exception.MathUnsupportedOperationException;
 import org.apache.commons.math4.exception.MaxCountExceededException;
 import org.apache.commons.math4.exception.util.LocalizedFormats;
 import org.apache.commons.math4.util.FastMath;
-import org.apache.commons.numbers.core.Precision;
 
 /**
  * Calculates the eigen decomposition of a real matrix.
