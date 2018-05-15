01e3fe13de3c3610bbb77ed9a349669c43950416
adrian <adrian-sw@aporter.org>
2018-05-15 16:40:20 -0400

    [~erans] suggests we derive the unit test from nothing.
    It tests the analytic solution against the new method.
    
    erans also suggested we move the BevingtonProblem into its own class.

--- src/test/java/org/apache/commons/math4/fitting/leastsquares/LevenbergMarquardtOptimizerTest.java
+++ src/test/java/org/apache/commons/math4/fitting/leastsquares/LevenbergMarquardtOptimizerTest.java
@@ -17,15 +17,8 @@
 
 package org.apache.commons.math4.fitting.leastsquares;
 
-import org.apache.commons.math4.analysis.MultivariateMatrixFunction;
-import org.apache.commons.math4.analysis.MultivariateVectorFunction;
 import org.apache.commons.math4.exception.DimensionMismatchException;
 import org.apache.commons.math4.exception.TooManyEvaluationsException;
-import org.apache.commons.math4.fitting.leastsquares.LeastSquaresBuilder;
-import org.apache.commons.math4.fitting.leastsquares.LeastSquaresOptimizer;
-import org.apache.commons.math4.fitting.leastsquares.LeastSquaresProblem;
-import org.apache.commons.math4.fitting.leastsquares.LevenbergMarquardtOptimizer;
-import org.apache.commons.math4.fitting.leastsquares.ParameterValidator;
 import org.apache.commons.math4.fitting.leastsquares.LeastSquaresOptimizer.Optimum;
 import org.apache.commons.math4.fitting.leastsquares.LeastSquaresProblem.Evaluation;
 import org.apache.commons.math4.geometry.euclidean.twod.Cartesian2D;
@@ -38,9 +31,6 @@ import org.apache.commons.math4.util.FastMath;
 import org.apache.commons.numbers.core.Precision;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 
@@ -357,58 +347,4 @@ public class LevenbergMarquardtOptimizerTest
         Assert.assertThat(optimum.getEvaluations(), is(2));
     }
 
-    protected static class BevingtonProblem {
-        private List<Double> time;
-        private List<Double> count;
-
-        public BevingtonProblem() {
-            time = new ArrayList<>();
-            count = new ArrayList<>();
-        }
-
-        public void addPoint(double t, double c) {
-            time.add(t);
-            count.add(c);
-        }
-
-        public MultivariateVectorFunction getModelFunction() {
-            return new MultivariateVectorFunction() {
-                @Override
-                public double[] value(double[] params) {
-                    double[] values = new double[time.size()];
-                    for (int i = 0; i < values.length; ++i) {
-                        final double t = time.get(i);
-                        values[i] = params[0] +
-                            params[1] * FastMath.exp(-t / params[3]) +
-                            params[2] * FastMath.exp(-t / params[4]);
-                    }
-                    return values;
-                }
-            };
-        }
-
-        public MultivariateMatrixFunction getModelFunctionJacobian() {
-            return new MultivariateMatrixFunction() {
-                @Override
-                public double[][] value(double[] params) {
-                    double[][] jacobian = new double[time.size()][5];
-
-                    for (int i = 0; i < jacobian.length; ++i) {
-                        final double t = time.get(i);
-                        jacobian[i][0] = 1;
-
-                        final double p3 =  params[3];
-                        final double p4 =  params[4];
-                        final double tOp3 = t / p3;
-                        final double tOp4 = t / p4;
-                        jacobian[i][1] = FastMath.exp(-tOp3);
-                        jacobian[i][2] = FastMath.exp(-tOp4);
-                        jacobian[i][3] = params[1] * FastMath.exp(-tOp3) * tOp3 / p3;
-                        jacobian[i][4] = params[2] * FastMath.exp(-tOp4) * tOp4 / p4;
-                    }
-                    return jacobian;
-                }
-            };
-        }
-    }
 }
