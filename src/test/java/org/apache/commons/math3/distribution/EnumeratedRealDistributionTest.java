ae452dd8bc48eaf4fc7246562039d29c19168dd7
Stephen Colebourne <scolebourne@joda.org>
2019-09-16 13:48:44 +0100

    Provide leniency in test case
    Test previously failed with 0.5 compared to 0.5000000000000001

--- src/test/java/org/apache/commons/math3/distribution/EnumeratedRealDistributionTest.java
+++ src/test/java/org/apache/commons/math3/distribution/EnumeratedRealDistributionTest.java
@@ -261,7 +261,7 @@ public class EnumeratedRealDistributionTest {
     public void testCreateFromDoubles() {
         final double[] data = new double[] {0, 1, 1, 2, 2, 2};
         EnumeratedRealDistribution distribution = new EnumeratedRealDistribution(data);
-        assertEquals(0.5, distribution.probability(2), 0);
+        assertEquals(0.5, distribution.probability(2), 1e-12);
         assertEquals(0.5, distribution.cumulativeProbability(1), 0);
     }
 }
