0b2f8f1a1c0c816701d405e08482a96336c0eb99
avbasak1 <avbasak1@in.ibm.com>
2022-01-15 14:10:28 +0530

    Fixed logger class names.

--- commons-math-ga/src/main/java/org/apache/commons/math4/ga/GeneticAlgorithm.java
+++ commons-math-ga/src/main/java/org/apache/commons/math4/ga/GeneticAlgorithm.java
@@ -35,7 +35,7 @@ import org.slf4j.LoggerFactory;
 public class GeneticAlgorithm<P> extends AbstractGeneticAlgorithm<P> {
 
     /** instance of logger. **/
-    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractGeneticAlgorithm.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(GeneticAlgorithm.class);
 
     /** crossover rate string. **/
     private static final String CROSSOVER_RATE = "CROSSOVER_RATE";
