2a32f7416c2f3ac9d50fc134f4c7a698dc71bdf9
avijit-basak <02508P744@host.docker.internal>
2022-04-08 13:29:02 +0530

    Fixed the fitness function for TSP.

--- commons-math-examples/examples-ga/examples-ga-tsp/src/main/java/org/apache/commons/math4/examples/ga/tsp/TSPOptimizer.java
+++ commons-math-examples/examples-ga/examples-ga-tsp/src/main/java/org/apache/commons/math4/examples/ga/tsp/TSPOptimizer.java
@@ -87,12 +87,9 @@ public final class TSPOptimizer {
                     ChromosomeRepresentationUtils.randomPermutation(cities.size()), decodedChromosome -> {
                 final DistanceMatrix distanceMatrix = DistanceMatrix.getInstance(cities);
                 double totalDistance = 0.0;
-                int index1 = 0;
-                int index2 = 0;
                 for (int j = 0; j < cities.size(); j++) {
-                    index1 = j;
-                    index2 = (j == cities.size() - 1) ? 0 : j + 1;
-                    totalDistance += distanceMatrix.getDistance(cities.get(index1), cities.get(index2));
+                    totalDistance += distanceMatrix.getDistance(decodedChromosome.get(j),
+                            decodedChromosome.get((j == cities.size() - 1) ? 0 : j + 1));
                 }
                 return -totalDistance;
             }, new RandomKeyDecoder<City>(cities)));
