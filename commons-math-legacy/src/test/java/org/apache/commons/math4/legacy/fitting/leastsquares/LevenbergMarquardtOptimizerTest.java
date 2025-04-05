0235de6ce94e2594dbf0b8d02e3ce787782932e7
Alex Herbert <aherbert@apache.org>
2025-04-05 08:09:17 +0100

    Use 2 * standard error for the radius
    This should address repeat failures of this test during builds.

--- commons-math-legacy/src/test/java/org/apache/commons/math4/legacy/fitting/leastsquares/LevenbergMarquardtOptimizerTest.java
+++ commons-math-legacy/src/test/java/org/apache/commons/math4/legacy/fitting/leastsquares/LevenbergMarquardtOptimizerTest.java
@@ -270,7 +270,7 @@ public class LevenbergMarquardtOptimizerTest
         // Check that the parameters are found within the assumed error bars.
         Assert.assertEquals("Delta=" + 2 * asymptoticStandardErrorFound[0], xCenter, paramFound[0], 2 * asymptoticStandardErrorFound[0]);
         Assert.assertEquals("Delta=" + 2 * asymptoticStandardErrorFound[1], yCenter, paramFound[1], 2 * asymptoticStandardErrorFound[1]);
-        Assert.assertEquals("Delta=" + 2 * asymptoticStandardErrorFound[2], radius, paramFound[2], asymptoticStandardErrorFound[2]);
+        Assert.assertEquals("Delta=" + 2 * asymptoticStandardErrorFound[2], radius, paramFound[2], 2 * asymptoticStandardErrorFound[2]);
     }
 
     @Test
